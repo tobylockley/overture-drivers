df8c420095d058c3940dc655915f06bfb0532431
Toby-AVD <toby@avdistributors.com.au>
2018-07-19 16:05:50 +1000

    update. still not totally happy with the implementation.

--- dev/extron_usb/index.js
+++ dev/extron_usb/index.js
@@ -1,90 +1,147 @@
+
+
+const CMD_DEFER_TIME = 1000
+const POLL_PERIOD = 10000
+const TICK_PERIOD = 5000
+const TELNET_TIMEOUT = 60000
+const TCP_TIMEOUT = 60000
+const TCP_RECONNECT_DELAY = 5000
+
+
 let host
 exports.init = _host => {
   host = _host
 }
 
+
 exports.createDevice = base => {
   const logger = base.logger || host.logger
   let config
-  let tcpClient
+  let commsClient
+  let zyperLastChangeId = 0;
+  let Telnet = require('telnet-client');
 
-  let frameParser = host.createFrameParser()
-  frameParser.setSeparator('\r\n')
-  frameParser.on('data', data => onFrame(data))
+  let frameParser = host.createFrameParser();
+  frameParser.on('data', data => onFrame(data));
+
+  let frameParser_rs232 = host.createFrameParser();  // Used to process data from zyper rs232 responses
+  frameParser_rs232.on('data', data => onFrame_rs232(data));
+  frameParser_rs232.setSeparator('\r\n');
+
 
   const setup = _config => {
     config = _config
+    if (config.interface === 'GlobalCache') frameParser.setSeparator('\r\n');
+    else if (config.interface === 'Zyper') frameParser.setSeparator('Zyper$');
     base.createVariable({
-      name: 'UsbInput',
+      name: 'UsbHost',
       type: 'integer',
       min: 0,
-      max: config.inputs,
+      max: config.usb_hosts,
       perform: {
-        action: 'Select Input',
+        action: 'Select Usb Host',
         params: {
-          Input: '$value'
+          Host: '$value'
         }
       }
     })
-    base.setPoll('Get Input', 5000)
+    base.setPoll({
+      action: 'getUsbHost',
+      period: POLL_PERIOD,
+      enablePollFn: () => { return base.getVar('Status').string === 'Connected' }
+    })
+    base.setTickPeriod(TICK_PERIOD);
   }
+
+
+  const tick = () => {
+    !commsClient && initComms();
+  }
+
 
   const start = () => {
-    initTcpClient()
-    tcpClient.connect(config.port, config.host)
-    base.startPolling()
+    initComms();
   }
+
 
   const stop = () => {
-    disconnect();
+    base.getVar('Status').string = 'Disconnected';
+    if (commsClient) {
+      commsClient.end();
+      commsClient = null;
+    }
   }
 
-  const disconnect = () => {
-    base.getVar('Status').string = 'Disconnected'
-  }
 
-  const initTcpClient = () => {
-    if (!tcpClient) {
-      tcpClient = host.createTCPClient()
-      tcpClient.setOptions({
-        autoReconnectionAttemptDelay: 5000,
-        receiveTimeout: 60000
+  const initComms = () => {
+    if (!commsClient) {
+      if (config.interface === 'GlobalCache') {
+        commsClient = host.createTCPClient();
+        commsClient.setOptions({
+          autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY,
+          receiveTimeout: TCP_TIMEOUT
+        });
+        commsClient.connect({host: config.host, port: config.port});
+      }
+      else if (config.interface === 'Zyper') {
+        commsClient = new Telnet();
+        commsClient.connect({ host: config.host, port: config.port, timeout: TELNET_TIMEOUT, initialLFCR: true });
+      }
+
+      commsClient.on('connect', () => {
+        logger.silly(`commsClient connected`);
+        base.getVar('Status').string = 'Connected';
+        base.startPolling();
       })
 
-      tcpClient.on('connect', () => {
-        logger.silly(`TCPClient connected`)
-        base.getVar('Status').string =  'Connected'
+      commsClient.on('data', data => {
+        frameParser.push(data.toString());
       })
 
-      tcpClient.on('data', data => {
-        onFrame(data)
+      commsClient.on('close', () => {
+        logger.silly(`commsClient closed`);
+        base.getVar('Status').string = 'Disconnected';
       })
 
-      tcpClient.on('close', () => {
-        logger.silly(`TCPClient closed`)
-        disconnect()
-      })
-
-      tcpClient.on('error', err => {
-        logger.error(`TCPClient: ${err}`)
-        disconnect()
-        tcpClient && tcpClient.end()
+      commsClient.on('error', err => {
+        logger.error(`commsClient: ${err}`);
+        stop();
       })
     }
   }
 
-  const send = data => {
-    logger.silly(`TCPClient send: ${data}`)
-    return tcpClient && tcpClient.write(data)
+
+  const send = (data) => {
+    logger.silly(`commsClient send: ${data}`);
+    if (config.interface === 'GlobalCache') {
+      return commsClient && commsClient.write(data);
+    }
+    else if (config.interface === 'Zyper') {
+      return commsClient && commsClient.send(`send ${config.zyper_device} rs232 ${data}\r\n`);
+    }
   }
+
 
   const sendDefer = data => {
-    if (send(data)) {
-      base.commandDefer(1000)
-    } else {
-      base.commandError(`Data not sent`)
+    logger.silly(`commsClient sendDefer: ${data}`);
+    base.commandDefer(CMD_DEFER_TIME);
+    if (config.interface === 'GlobalCache') {
+      if (!commsClient.write(data)) base.commandError(`TCP write error!`);
+    }
+    else if (config.interface === 'Zyper') {
+      commsClient.send(`send ${config.zyper_device} rs232 ${data}\r\n`).then(result => {
+        // RS232 Response data currently not functioning for zyper device, this is a workaround     !!!!!!!!!!!!!!!!!!!
+        let match = data.match(/switch to (HDMI\d)/);
+        if (match) {
+          base.commandDone();
+          base.getVar('Sources').string = match[1];
+        }
+      }, err => {
+        base.commandError(`Telnet send error: ${err}`);
+      })
     }
   }
+
 
   const onFrame = data => {
     logger.silly(`onFrame: ${data}`)
@@ -93,22 +150,55 @@ exports.createDevice = base => {
       base.commandError()
     }
     else {
-      // The following syntax is the same for both selectInput and getInput response (see doc)
-      let match = data.match(/Chn(d+)/)
-      match && (base.getVar('UsbInput').value = parseInt(match[1]))
+      // Process zyper rz232 responses
+      if (data.includes('rs232Response')) {
+        base.commandDone();
+        let match, regex = /device\.rs232Response\.\d+; string="(.*?)"/g;
+        while (match = regex.exec(data)) {
+          frameParser_rs232.push(match[1]);
+        }
+      }
+      else {
+        // The following syntax is the same for both selectUsbHost and getUsbHost response (see doc)
+        let match = data.match(/Chn(\d+)/)
+        if (match) {
+          base.commandDone();
+          base.getVar('UsbHost').value = parseInt(match[1]);
+        }
+      }
     }
   }
 
-  const getInput = () => {
-    sendDefer('I')
+
+  const onFrame_rs232 = data => {
+    logger.info(`onFrame_rs232: ${data}`)
   }
 
-  const selectInput = params => {
-    sendDefer(`${params.Input}!`)
+
+  const getUsbHost = () => {
+    sendDefer('I');  // Handled in onFrame
+    if (config.interface === 'Zyper') {
+      // Get response data from zyper
+      commsClient.send(`show responses ${config.zyper_device} rs232 since ${zyperLastChangeId}\r\n`).then(result => {
+        let match = data.match(/switch to (HDMI\d)/);
+        if (match) {
+          base.commandDone();
+          base.getVar('Sources').string = match[1];
+        }
+      }, err => {
+        base.commandError(`Telnet send error: ${err}`);
+      })
+    }
   }
+
+
+  const selectUsbHost = params => {
+    sendDefer(`${params.Host}!`)
+  }
+
 
   return {
-    setup, start, stop,
-    getInput, selectInput
+    setup, start, stop, tick,
+    getUsbHost, selectUsbHost
   }
 }
