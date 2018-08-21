10aecbc8c8d16cb90e2faddf1240b2ed04e2eb93
Toby-AVD <toby@avdistributors.com.au>
2018-08-22 09:30:31 +1000

    Experimental changes

--- dev/dummy_sony_bravia/index.js
+++ dev/dummy_sony_bravia/index.js
@@ -7,16 +7,49 @@ exports.createDevice = base => {
   const logger = base.logger || host.logger
   let config
 
-  const setup = _config => {
+  function isConnected() { return base.getVar('Status').string === 'Connected' };
+
+  function setup(_config) {
     config = _config
+
+    base.setTickPeriod(6000);
+    base.setPoll('pollTest', 3000, {}, isConnected, false);
+
+    base.createVariable({
+      name: 'ForceConnection',
+      type: 'enum',
+      enums: ['Off', 'On'],
+      perform: {
+        action: 'forceConnection',
+        params: { Value: '$value' }
+      }
+    });
+  }
+
+  function tick() {
+    logger.debug('>>> TICK <<<')
+    // base.startPolling()
+    // if (base.getVar('Status').value == 0) base.stopPolling()
+  }
+
+  function pollTest() {
+    logger.info('====== poll test =====')
+  }
+
+  function forceConnection(params) {
+    let val = parseInt(params.Value)
+    base.getVar('Status').value = val;
+    base.getVar('ForceConnection').value = val;
   }
 
   const start = () => {
     base.getVar('Status').string = 'Connected';
+    base.startPolling()
   }
 
   const stop = () => {
     base.getVar('Status').string = 'Disconnected';
+    // base.stopPolling()
   }
 
   const setPower = params => {
@@ -48,7 +81,7 @@ exports.createDevice = base => {
   }
 
   return {
-    setup, start, stop,
-    setPower, selectSource, setAudioLevel, setAudioMute, setChannel, shiftChannel
+    setup, start, stop, tick,
+    setPower, selectSource, setAudioLevel, setAudioMute, setChannel, shiftChannel, pollTest, forceConnection
   }
 }
