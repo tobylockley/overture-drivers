793ec7bdfe403dcea85f6e2485fc8cf678a07079
Toby Lockley <toby@avdistributors.com.au>
2019-01-15 14:13:48 +1000

    Script Commands addon depricated, updated to use power tools

--- dev/.scripts/overture_zip.js
+++ dev/.scripts/overture_zip.js
@@ -1,20 +1,17 @@
 
 const DELETE_OLD_FILES = false;
 
-exports.execute = function (args) {
-  // access VSCode API (s. https://code.visualstudio.com/Docs/extensionAPI/vscode-api)
-  var vscode = require('vscode');
-  var fs = require('fs');
-  var path = require('path');
-  var archiver = require('archiver');
-  var glob = require('glob');
+exports.execute = async (args) => {
+  // args => https://egodigital.github.io/vscode-powertools/api/interfaces/_contracts_.workspacecommandscriptarguments.html
+  // https://code.visualstudio.com/api/references/vscode-api
+  const vscode = args.require('vscode');
+  const fs = require('fs');
+  const path = require('path');
+  const archiver = require('archiver');
+  const glob = require('glob');
 
   let mypath = path.dirname(vscode.window.activeTextEditor.document.fileName);
-  let zpath = path.resolve(mypath, '../../zip')
-
-  args.log('---------- ./dev/scripts/ozip.js ----------');
-  args.log(`input path: ${mypath}`);
-  args.log(`output path: ${zpath}`)
+  let zpath = path.resolve(mypath, '../../zip');
 
   fs.readdir(mypath, function(err, items) {
     if (!err) {
@@ -25,9 +22,8 @@ exports.execute = function (args) {
 
       let files = glob.sync(`${zpath}/${json.name}.*.zip`)
       if (files && DELETE_OLD_FILES) {
-        args.log('Deleting old zip files...')
+        vscode.window.showInformationMessage('overtureZip: Deleting old zip files...')
         files.forEach(file => {
-          args.log(`Deleting: ${file}`)
           fs.unlinkSync(file)
         });
       }
@@ -41,21 +37,21 @@ exports.execute = function (args) {
       // listen for all archive data to be written
       // 'close' event is fired only when a file descriptor is involved
       output.on('close', function() {
-        args.log(`Complete: Driver files archived to ${zpath}\\${zname}.zip (${archive.pointer()} bytes)`);
+        vscode.window.showInformationMessage(`overtureZip: Driver files archived to ${zpath}\\${zname}.zip (${archive.pointer()} bytes)`);
       });
 
       // This event is fired when the data source is drained no matter what was the data source.
       // It is not part of this library but rather from the NodeJS Stream API.
       // @see: https://nodejs.org/api/stream.html#stream_event_end
       output.on('end', function() {
-        args.log('Data has been drained');
+        vscode.window.showWarningMessage('Data has been drained');
       });
 
       // good practice to catch warnings (ie stat failures and other non-blocking errors)
       archive.on('warning', function(err) {
         if (err.code === 'ENOENT') {
           // log warning
-          console.log(err)
+          vscode.window.showErrorMessage(err.message);
         } else {
           // throw error
           throw err;
@@ -81,7 +77,7 @@ exports.execute = function (args) {
       // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
       archive.finalize();
     }
-    throw err
+    else throw err;
   });
-}
+};
 
