c83e12cd01ee362fe9356325b9aefbd8d5f824e1
Toby Lockley <toby@avdistributors.com.au>
2019-01-23 11:43:11 +1000

    Refactored to use inbuilt TCP instead of additional telnet module

--- dev/tl_4x1_hd2_zyper/index.js
+++ dev/tl_4x1_hd2_zyper/index.js
@@ -1,7 +1,10 @@
-const CMD_DEFER_TIME = 5000;
-const TICK_PERIOD = 5000;
-const POLL_PERIOD = 30000;
-const TELNET_TIMEOUT = 60000;
+'use strict';
+
+const CMD_DEFER_TIME = 2000;
+const TICK_PERIOD = 3000;
+const POLL_PERIOD = 10000;
+const TCP_TIMEOUT = 30000;          // Will timeout after this length of inactivity
+const TCP_RECONNECT_DELAY = 3000;   // How long to wait before attempting to reconnect
 
 
 let host;
@@ -13,23 +16,27 @@ exports.init = _host => {
 exports.createDevice = base => {
   const logger = base.logger || host.logger;
   let config;
-  let telnetClient;
-  let Telnet = require('telnet-client');
-  let zyperChangeIds = {};
+  let tcpClient;
 
   let frameParser = host.createFrameParser();
-  frameParser.setSeparator('\\r\\n');  // Separator for zyper rs232 data
+  frameParser.setSeparator('Zyper$');
   frameParser.on('data', data => onFrame(data));
 
+  let frameParser_rs232 = host.createFrameParser();
+  frameParser_rs232.setSeparator('\\r\\n');  // Separator for zyper rs232 data
+  // frameParser_rs232.on('data', data => onFrame(data));
+
+
+  // ------------------------------ SETUP FUNCTIONS ------------------------------
+
+  function isConnected() { return base.getVar('Status').string === 'Connected'; }
 
   function setup(_config) {
     config = _config;
-    base.setPoll({
-      action: 'keepAlive',
-      period: POLL_PERIOD,
-      enablePollFn: () => { return base.getVar('Status').string === 'Connected'; }
-    });
     base.setTickPeriod(TICK_PERIOD);
+
+    // base.setPoll({ action: 'keepAlive', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true });
+    // base.setPoll({ action: 'getSerialResponses', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true });
 
     config.switchers.forEach(switcher => {
       base.createVariable({
@@ -44,128 +51,148 @@ exports.createDevice = base => {
           }
         }
       });
-      zyperChangeIds[switcher.name] = 0;  // Initialise change ids
+      switcher.lastChangeId = 0;  // Used to track rs232 responses
+    });
+
+    base.createVariable({
+      name: 'TestString',
+      type: 'string',
+      perform: {
+        action: 'testCommand',
+        params: {
+          Command: '$string'
+        }
+      }
     });
   }
 
 
   function start() {
-    initTelnet();
+    initTcpClient();
   }
 
 
   function stop() {
     base.getVar('Status').string = 'Disconnected';
-    if (telnetClient) {
-      telnetClient.end();
-      telnetClient = null;
-    }
+    tcpClient && tcpClient.end();
+    tcpClient = null;
+    base.clearPendingCommands();
   }
 
-
-  // eslint-disable-next-line no-unused-vars
   function tick() {
-    !telnetClient && initTelnet();
+    if (!tcpClient) initTcpClient();
+  }
+
+  function initTcpClient() {
+    if (tcpClient) return;  // Return if tcpClient already exists
+
+    tcpClient = host.createTCPClient();
+    tcpClient.setOptions({
+      receiveTimeout: TCP_TIMEOUT,
+      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY
+    });
+    tcpClient.connect(config.port, config.host);
+
+    tcpClient.on('connect', () => {
+      logger.silly('TCPClient connected');
+      base.getVar('Status').string = 'Connected';
+      base.startPolling();
+    });
+
+    tcpClient.on('data', data => {
+      frameParser.push(data.toString());
+    });
+
+    tcpClient.on('close', () => {
+      logger.silly('TCPClient closed');
+      base.getVar('Status').string = 'Disconnected';  // Triggered on timeout, this allows auto reconnect
+    });
+
+    tcpClient.on('error', err => {
+      logger.error(`TCPClient: ${err}`);
+      stop();  // Throw out the tcpClient and get a fresh connection
+    });
   }
 
 
-  function initTelnet() {
-    if (!telnetClient) {
-      telnetClient = new Telnet();
-      telnetClient.connect({ host: config.host, port: config.port, timeout: TELNET_TIMEOUT, initialLFCR: true });
-
-      telnetClient.on('connect', () => {
-        logger.silly('telnetClient connected');
-        base.getVar('Status').string = 'Connected';
-        base.startPolling();
-        updateChangeIds();  // Update the lastChangeId from system
-      });
-
-      // eslint-disable-next-line no-unused-vars
-      telnetClient.on('data', data => {
-        //frameParser.push(data.toString());  // Being handled in sendDefer callback
-      });
-
-      telnetClient.on('close', () => {
-        logger.silly('telnetClient closed');
-        base.getVar('Status').string = 'Disconnected';
-      });
-
-      telnetClient.on('error', err => {
-        logger.error(`telnetClient: ${err}`);
-        stop();
-      });
-    }
-  }
-
+  // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
 
   function send(data) {
-    logger.silly(`telnetClient send: ${data}`);
-    return telnetClient && telnetClient.send(data);
+    logger.silly(`TCPClient send: ${data}`);
+    return tcpClient && tcpClient.write(data);
   }
 
-
-  function sendDefer(data, callback) {
-    logger.silly(`telnetClient sendDefer: ${data}`);
-    base.commandDefer(CMD_DEFER_TIME);
-    telnetClient.send(data).then(result => {
-      logger.silly(`sendDefer response: ${result}`);
-      if (result.includes('Success')) {
-        base.commandDone();
-        callback(result);
-      }
-      else base.commandError('Telnet response error');
-    }, err => {
-      base.commandError(`Telnet send error: ${err}`);
-    });
+  function sendDefer(data) {
+    if (send(data)) base.commandDefer(CMD_DEFER_TIME);
+    else base.commandError('Data not sent');
   }
-
 
   function onFrame(data) {
-    logger.silly(`onFrame: ${data}`);
-    // Not currently in use - not able to poll switchers for current output
-    let match = data.match(/switch to (HDMI\d)/);
-    if (match) {
-      base.getVar('Sources').string = match[1];
-      // base.commandDone();  // Handled in sendDefer
+    const pendingCommand = base.getPendingCommand();
+    pendingCommand && logger.silly(`onFrame pendingCommand: ${pendingCommand.action}`);
+    logger.debug(`onFrame: ${data}`);
+
+    if (pendingCommand && pendingCommand.action == 'keepAlive') {
+      base.commandDone();
     }
+    else if (pendingCommand && pendingCommand.action == 'selectSource') {
+      base.commandDone();
+      getSerialResponses(pendingCommand.params.Channel);
+    }
+    else if (pendingCommand && pendingCommand.action == 'getSerialResponses') {
+      base.commandDone();
+    }
+
+    // Not currently in use - not able to poll switchers for current output
+    // let match = data.match(/switch to (HDMI\d)/);
+    // if (match) {
+      // base.getVar('Sources').string = match[1];
+      // base.commandDone();  // Handled in sendDefer
+    // }
+
+    // base.commandDone();
+
   }
 
+
+  // ------------------------------ GET FUNCTIONS ------------------------------
 
   function keepAlive() {
-    send('show server config\r\n');  // Send rubbish to keep the tcp connection alive
+    sendDefer('\n');  // Send rubbish to keep the tcp connection alive
   }
 
-
-  function updateChangeIds() {
-    // Update RS232 change ids from each registered device
-    for (let device in zyperChangeIds) {
-      sendDefer(`show responses ${config.zyper_device} rs232 last-change-id\r\n`, response => {
-        let match = response.match(/lastChangeId\((\d+)\);/);
-        if (match) {
-          zyperChangeIds[device] = parseInt(match[1]);  // Update last changeId to keep track of responses
-          logger.silly(`Updating lastChangeId for '${device}' = ${match[1]}`);
-        }
-      });
+  function getSerialResponses(device) {
+    let switcher = config.switchers.filter(switcher => switcher.zyper_device == device);
+    if (switcher.length != 1) {
+      logger.error(`getSerialResponses: Could not find single entry for zyper device named "${device}"`);
+      return;
     }
+    switcher = switcher[0];  // If we got here, there is only 1 result, so extract from array
+    sendDefer(`show responses ${switcher.zyper_device} rs232 since ${switcher.lastChangeId}\n`);
   }
 
+
+  // ------------------------------ SET FUNCTIONS ------------------------------
 
   function selectSource(params) {
-    // params.Name = source, e.g. "HDMI"
     // params.Channel = name/mac of zyper device, e.g. "4k_dec1"
-    sendDefer(`send ${params.Channel} rs232 ${params.Name}%\r\n`, response => {
-      let match = response.match(/(HDMI\d)/);
-      if (match) {
-        base.getVar('Sources').string = match[1];
-        // base.commandDone();  // Handled in sendDefer
-      }
-    });
+    // params.Name = source, e.g. "HDMI1"
+    sendDefer(`send ${params.Channel} rs232 ${params.Name}%\n`);
   }
 
 
+  function testCommand(params) {
+    // send the string to rs232 device 1
+    sendDefer(`send uhd_enc1 rs232 ${params.Command}\n`);
+  }
+
+
+  // ------------------------------ EXPORTED FUNCTIONS ------------------------------
+
   return {
-    setup, start, stop,
-    keepAlive, selectSource
+    setup, start, stop, tick,
+    keepAlive, getSerialResponses,
+    selectSource,
+    testCommand
   };
 };
