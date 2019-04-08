df4b8b00c596ff68296a546fa5e8caca0f8a142d
Toby Lockley <toby@avdistributors.com.au>
2019-04-08 15:07:49 +1000

    Internal testing for UniSee

--- dev/barco_display_bcm_videowall/index.js
+++ dev/barco_display_bcm_videowall/index.js
@@ -1,3 +1,272 @@
+'use strict';
+
+const CMD_DEFER_TIME = 1000;        // Timeout when using commandDefer
+const TICK_PERIOD = 5000;           // In-built tick interval
+const POLL_PERIOD = 5000;           // Continuous polling function interval
+const TCP_TIMEOUT = 30000;          // Will timeout after this length of inactivity
+const TCP_RECONNECT_DELAY = 3000;   // How long to wait before attempting to reconnect
+
+let host;
+exports.init = _host => {
+  host = _host;
+};
+
+exports.createDevice = base => {
+  const logger = base.logger || host.logger;
+  let config;
+  let tcpClient;
+
+  let frameParser = host.createFrameParser();
+  frameParser.setSeparator('\r\n');
+  frameParser.on('data', data => onFrame(data));
+
+  let wall_modules = [];  // This will store BCM wall modules (1,1 2,1 etc)
+
+  // ------------------------------ SETUP FUNCTIONS ------------------------------
+
+  function isConnected() { return base.getVar('Status').string === 'Connected'; }
+  function isPoweredOn() { return isConnected() && base.getVar('Power').string === 'On'; }
+
+  function setup(_config) {
+    config = _config;
+    base.setTickPeriod(TICK_PERIOD);
+
+    // Register polling functions
+    base.setPoll({ action: 'getPower', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true });
+    base.setPoll({ action: 'getAllSources', period: POLL_PERIOD, enablePollFn: isPoweredOn, startImmediately: true });
+  }
+
+  function start() {
+    initTcpClient();
+  }
+
+  function tick() {
+    if (!tcpClient) initTcpClient();
+  }
+
+  function stop() {
+    base.getVar('Status').string = 'Disconnected';
+    tcpClient && tcpClient.end();
+    tcpClient = null;
+    base.stopPolling();
+    base.clearPendingCommands();
+  }
+
+  function initTcpClient() {
+    if (tcpClient) return;  // Return if tcpClient already exists
+
+    tcpClient = host.createTCPClient();
+    tcpClient.setOptions({
+      receiveTimeout: TCP_TIMEOUT,
+      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY
+    });
+    tcpClient.connect(config.port, config.host);
+
+    tcpClient.on('connect', () => {
+      logger.silly('TCPClient connected');
+      base.getVar('Status').string = 'Connected';
+      sendDefer(`${config.device_id} get WallModules`);  // On connection, get wall modules (e.g. 1,1 1,2 etc)
+      base.startPolling();
+    });
+
+    tcpClient.on('data', data => {
+      frameParser.push( data.toString() );
+    });
+
+    tcpClient.on('close', () => {
+      logger.silly('TCPClient closed');
+      base.getVar('Status').string = 'Disconnected';  // Triggered on timeout, this allows auto reconnect
+    });
+
+    tcpClient.on('error', err => {
+      logger.error(`TCPClient: ${err}`);
+      stop();  // Throw out the tcpClient and get a fresh connection
+    });
+  }
+
+
+  // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
+
+  function send(data) {
+    logger.silly(`TCPClient send: ${data}`);
+    return tcpClient && tcpClient.write(data + '\r\n');  // Append with newline
+  }
+
+  function sendDefer(data) {
+    if (send(data)) base.commandDefer(CMD_DEFER_TIME);
+    else base.commandError('Data not sent');
+  }
+
+  function onFrame(data) {
+    let match;  // Used for regex matching below
+    const pendingCommand = base.getPendingCommand();
+
+    logger.silly(`onFrame: ${data}`);
+    pendingCommand && logger.debug(`pendingCommand: ${pendingCommand.action}`);
+
+    match = data.match(/get WallModules \{(.*?)\}/i);
+    if (match) {
+      wall_modules = match[1].split(' ');  // Split modules into an array
+      base.commandDone();
+    }
+
+    match = data.match(/get SelInputs .+?\} (.+)/i);
+    if (match) {
+      let inputs = match[1];  // This should be similar to "HDMI1 {2,2 2,1 1,2} HDMI2 {1,1}"
+      let regex_inputs = /(\w+?) \{(.+?)\}/g;
+      let input_match;
+      while ((input_match = regex_inputs.exec(inputs)) != null) {
+        let source = input_match[1];
+        let modules = input_match[2].split(' ');
+        for (let module of modules) {
+          base.getVar(`Sources_${module.replace()}`)
+        }
+      }
+
+      base.getVar('ChannelShift').value = 0;  // Reset to 'idle'
+      base.commandDone();
+    }
+
+  }
+
+
+  // ------------------------------ GET FUNCTIONS ------------------------------
+  function getPower() {
+    sendDefer('get OpState wall');
+  }
+
+  function getAllSources() {
+    sendDefer('get SelInput wall');
+  }
+
+
+
+
+//DEVICE COMMANDS-----------------------------------------------------------
+
+//END DEVICE COMMANDS------------------------------------------------
+// tick() {} // Using own setInterval instead
+
+//POLL COMMANDS--------------------------------------------
+
+
+
+
+
+  // ------------------------------ SET FUNCTIONS ------------------------------
+setPower(params) {
+  let power = params.Status === 'On'? "on" : "idle";
+  let msg = 'set OpState wall ' + power;
+  if(this.sendCommand(msg)) this.base.commandDefer(2000);
+  else this.base.commandError('Not Sent');
+}
+selectSource(params) {
+  let msg = 'set SelInput wall ' + params.Name;
+  if(this.sendCommand(msg)) this.base.commandDefer(2000);
+  else this.base.commandError('Not Sent');
+}
+
+
+
+
+  function setPower(params) {
+    if (config.simulation) {
+      base.getVar('Power').string = params.Status;
+      return;
+    }
+
+    if (params.Status == 'Off') sendDefer('*SCPOWR0000000000000000\n');
+    else if (params.Status == 'On') sendDefer('*SCPOWR0000000000000001\n');
+  }
+
+  function selectSource(params) {
+    if (config.simulation) {
+      base.getVar('Sources').string = params.Name;
+      return;
+    }
+
+    if (params.Name == 'DTV') sendDefer('*SCINPT0000000000000000\n');
+    else {
+      let match = params.Name.match(/HDMI(\d)/);
+      match && sendDefer(`*SCINPT000000010000000${match[1]}\n`);
+    }
+  }
+
+  function setAudioLevel(params) {
+    if (config.simulation) {
+      base.getVar('AudioLevel').value = params.Level;
+      return;
+    }
+
+    let vol = params.Level.toString().padStart(3, '0');  // Formats the integer with leading zeroes, e.g. 53 = '053'
+    sendDefer(`*SCVOLU0000000000000${vol}\n`);
+  }
+
+  function setAudioMute(params) {
+    if (config.simulation) {
+      base.getVar('AudioMute').string = params.Status;
+      return;
+    }
+
+    if (params.Status == 'Off') sendDefer('*SCAMUT0000000000000000\n');
+    else if (params.Status == 'On') sendDefer('*SCAMUT0000000000000001\n');
+  }
+
+  function setChannel(params) {
+    if (isDTVMode()) {
+      if (config.simulation) {
+        base.getVar('Channel').value = params.Name;
+        return;
+      }
+      let channel = params.Name.toString().padStart(8, '0');
+      sendDefer(`*SCCHNN${channel}.0000000\n`);
+    }
+    else {
+      logger.error('Cannot change channel unless set to DTV mode');
+    }
+  }
+
+  function shiftChannel(params) {
+    if (isDTVMode()) {
+      if (config.simulation) {
+        let delta = { 'Up': 1, 'Down': -1 };
+        base.getVar('Channel').value += delta[params.Name];
+        return;
+      }
+      if (params.Name == 'Up') sendDefer('*SCIRCC0000000000000033\n');
+      else if (params.Name == 'Down') sendDefer('*SCIRCC0000000000000034\n');
+      base.getVar('ChannelShift').string = params.Name;
+    }
+    else {
+      logger.error('Cannot change channel unless set to DTV mode');
+    }
+  }
+
+
+  // ------------------------------ EXPORTED FUNCTIONS ------------------------------
+  return {
+    setup, start, stop, tick,
+    getPower, getSource, getAudioLevel, getAudioMute, getChannel,
+    setPower, selectSource, setAudioLevel, setAudioMute, setChannel, shiftChannel
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use strict"
 
 // exported init function
@@ -75,7 +344,7 @@ getSource(params){
 //END POLL COMMANDS----------------------------------------
 //UTIL COMMANDS---------------------------------
   sendCommand(msg){
-    return this.tcpClient.write(this.config.deviceID + " " + msg + "\r\n");
+    return this.tcpClient.write(this.config.device_id + " " + msg + "\r\n");
   }
   startPoll(){
     this.base.perform('getPower');
