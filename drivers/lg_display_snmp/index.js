e27cee09b123696f6570486b51370561bde2db1d
Toby Lockley <toby@avdistributors.com>
2019-06-27 17:46:29 +1000

    LG SNMP working version

--- drivers/lg_display_snmp/index.js
+++ drivers/lg_display_snmp/index.js
@@ -3,8 +3,7 @@
 const CMD_DEFER_TIME = 3000        // Timeout when using commandDefer
 const TICK_PERIOD = 5000           // In-built tick interval
 const POLL_PERIOD = 5000           // Continuous polling function interval
-const TCP_TIMEOUT = 30000          // Will timeout after this length of inactivity
-const TCP_RECONNECT_DELAY = 1000   // How long to wait before attempting to reconnect
+const SNMP_TIMEOUT = 2000          // SNMP request timeout
 
 let host
 exports.init = _host => {
@@ -14,14 +13,13 @@ exports.init = _host => {
 exports.createDevice = base => {
   const logger = base.logger || host.logger
   let config
-  let tcpClient
+  let networkUtilities  // Used for pinging device
+  let snmp = require('snmp-native')
+  let snmpSession
   let wol = require('wol')  // Used to turn wake from power off, must be enabled in menu
   let sourcesInfo = []  // Used to store inputs sources hex codes
-  let irCodes  // Populated during setup
-
-  let frameParser = host.createFrameParser()
-  frameParser.setSeparator('x')
-  frameParser.on('data', data => onFrame(data))
+  let irCodes  // Populated during setup from ir_codes.json
+  let oids  // Populated during setup from oids.json
 
 
   // ------------------------------ SETUP FUNCTIONS ------------------------------
@@ -30,13 +28,16 @@ exports.createDevice = base => {
 
   function setup(_config) {
     config = _config
+
+    // Create IR Commands enum based on ir_codes.json
+    irCodes = importIrCodes('./ir_codes.json')
+    base.getVar('IRCommands').enums = ['Choose a Command'].concat(Object.keys(irCodes))
+
+    // Load SNMP OID values in array format
+    oids = importOids('./oids.json')
+
     base.setTickPeriod(TICK_PERIOD)
-    base.setPoll({ action: 'getPower', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-    base.setPoll({ action: 'getScreenMute', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-    base.setPoll({ action: 'getSource', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-    base.setPoll({ action: 'getAudioMute', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-    base.setPoll({ action: 'getAudioLevel', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-    base.setPoll({ action: 'getTemperature', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
+    base.setPoll({ action: 'getAll', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
 
     // Load this drivers package.json and get sources title and hexcode, add only if enabled
     let sourcesData = require('./package.json').overture.pointSetupSchema.properties.inputs.properties
@@ -48,218 +49,94 @@ exports.createDevice = base => {
       }
     }
     base.getVar('Sources').enums = sourcesInfo.map(x => x.title)
-
-    // Create IR Commands enum based on ir_codes.json
-    irCodes = require('./ir_codes.json')
-    base.getVar('IRCommands').enums = ["Choose a Command"].concat(Object.keys(irCodes))
-
-    // Create videowall variables and set polls if enabled
-    if (config.videowall) {
-      base.createVariable({
-        name: 'TileMode',
-        type: 'enum',
-        enums: [
-          "Off",
-          "1x2",
-          "2x2",
-          "3x3",
-          "4x4",
-          "5x5"
-        ],
-        perform: {
-          action: 'setTileMode',
-          params: { Status: '$string' }
-        }
-      })
-      base.createVariable({
-        name: 'TileId',
-        type: 'integer',
-        min: 1,
-        max: 25,
-        perform: {
-          action: 'setTileId',
-          params: { Value: '$value' }
-        }
-      })
-      base.setPoll({ action: 'getTileMode', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-      base.setPoll({ action: 'getTileId', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-    }
   }
 
   function start() {
-    initTcpClient()
+    networkUtilities = host.createNetworkUtilities()
+    base.startPolling()
+    snmpSession = new snmp.Session({
+      host: config.host,
+      port: config.port,
+      community: 'lgecommer',
+      timeouts: [SNMP_TIMEOUT]
+    })
   }
 
   function stop() {
-    tcpClient && tcpClient.end()
-    tcpClient = null
+    networkUtilities = null
     base.stopPolling()
     base.clearPendingCommands()
+    snmpSession.close()
+    snmpSession = null
   }
 
   function tick() {
-    if (!tcpClient) initTcpClient()
-  }
-
-  function initTcpClient() {
-    if (tcpClient) return  // Return if tcpClient already exists
-
-    tcpClient = host.createTCPClient()
-    tcpClient.setOptions({
-      receiveTimeout: TCP_TIMEOUT,
-      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY
-    })
-    tcpClient.connect(config.port, config.host)
-
-    tcpClient.on('connect', () => {
-      logger.silly('TCPClient connected')
-      base.getVar('Status').string = 'Connected'
-      base.startPolling()
-    })
-
-    tcpClient.on('data', data => {
-      frameParser.push(data.toString())
-    })
-
-    tcpClient.on('close', () => {
-      logger.silly('TCPClient closed')
-      base.getVar('Status').string = 'Disconnected'  // Triggered on timeout, this allows auto reconnect
-      base.getVar('Power').value = 0  // Allow for WOL to be sent
-    })
-
-    tcpClient.on('error', err => {
-      logger.error(`TCPClient: ${err}`)
-      stop()  // Throw out the tcpClient and get a fresh connection
-    })
-  }
-
-
-  // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
-
-  function send(data) {
-    logger.silly(`TCPClient send: ${data}`)
-    return tcpClient && tcpClient.write(data)
-  }
-
-  function sendDefer(data) {
-    base.commandDefer(CMD_DEFER_TIME)
-    if (!send(data)) base.commandError('Data not sent')
-  }
-
-  function onFrame(data) {
-    const pendingCommand = base.getPendingCommand()
-    logger.silly(`onFrame (pending = ${pendingCommand && pendingCommand.action}): ${data}`)
-
-    let match = data.match(/(\w) (\d+) OK([0-9a-fA-F]+)/)
-    if (match && pendingCommand) {
-      let id = parseInt(match[2], 16)
-      let val = parseInt(match[3], 16)
-      if (id === config.setID) {
-        if (match[1] === 'a') {
-          base.getVar('Power').value = val
-          base.commandDone()
-          if (val === 0) {
-            // Stop all other polling commands, and set to disconnected
-            // setTimeout is needed to perform at end of event loop
-            setTimeout(() => { base.clearPendingCommands() }, 0)
-            base.getVar('Status').string = 'Disconnected'
-          }
+    // Check network connection
+    networkUtilities.ping(config.host, {timeout: SNMP_TIMEOUT})
+      .then(result => {
+        if (result == true && base.getVar('Status').value === 0) {
+          base.getVar('Status').value = 1  // Set to connected
+          base.getVar('Power').value = 1  // Connection means power is on
         }
-        else if (match[1] === 'd') {
-          // Could be screen mute or tile mode set
-          if (pendingCommand.action === 'setTileMode') {
-            base.getVar('TileMode').string = pendingCommand.params.Status
-            base.commandDone()
-          }
-          else {
-            base.getVar('ScreenMute').value = val
-            base.commandDone()
-          }
+        else if (result == false) {
+          base.getVar('Status').value = 0  // Set to disconnected
+          base.getVar('Power').value = 0  // Set power off to allow WOL
         }
-        else if (match[1] === 'b') {
-          let source = sourcesInfo.find(x => x.hexcode === val)
-          if (source) {
-            base.getVar('Sources').string = source.title
-            base.commandDone()
-          }
-          else {
-            base.commandError(`Could not find current source hexcode (${val.toString(16)}) in available sources`)
-          }
-        }
-        else if (match[1] === 'e') {
-          base.getVar('AudioMute').string = ['On', 'Off'][val]  // 0 = Muted, 1 = Unmuted
-          base.commandDone()
-        }
-        else if (match[1] === 'f') {
-          base.getVar('AudioLevel').value = val
-          base.commandDone()
-        }
-        else if (match[1] === 'c') {
-          base.getVar('IRCommands').value = 0  // Reset to idle state
-          base.commandDone()
-        }
-        else if (match[1] === 'n') {
-          base.getVar('Temperature').value = val
-          base.commandDone()
-        }
-        else if (match[1] === 'z' && base.getVar('TileMode')) {
-          let bytes = match[3].match(/.{2}/g).map(x => parseInt(x, 16))
-          let tileMode = bytes[0]
-          let cols = bytes[1]
-          let rows = bytes[2]
-          let tileString = (tileMode > 0) ? `${rows}x${cols}` : 'Off'
-          base.getVar('TileMode').string = tileString
-          base.commandDone()
-        }
-        else if (match[1] === 'i' && base.getVar('TileId')) {
-          base.getVar('TileId').value = val
-          base.commandDone()
-        }
-        else {
-          base.commandError(`Unable to process received data: ${data}`)
-        }
-      }
-      else {
-        base.commandError(`onFrame: Set ID received (${id}) does not match configured ID (${config.setID})`)
-      }
-    }
-    else if (match && !pendingCommand) {
-      logger.warn(`Received data but no pending command: ${data}`)
-    }
-    else {
-      logger.warn(`onFrame data not processed`)
-    }
+      })
+      .catch(error => {
+        logger.error(`Ping Error: ${error.message}`)
+      })
   }
 
 
   // ------------------------------ GET FUNCTIONS ------------------------------
 
-  function getPower() { sendDefer(`ka ${config.setID} FF\r`) }
-  function getSource() { sendDefer(`xb ${config.setID} FF\r`) }
-  function getScreenMute() { sendDefer(`kd ${config.setID} FF\r`) }
-  function getAudioMute() { sendDefer(`ke ${config.setID} FF\r`) }
-  function getAudioLevel() { sendDefer(`kf ${config.setID} FF\r`) }
-  function getTemperature() { sendDefer(`dn ${config.setID} FF\r`) }
-  function getTileMode() { sendDefer(`dz ${config.setID} FF\r`) }
-  function getTileId() {
-    if (base.getVar('TileMode').value > 0) {
-      sendDefer(`di ${config.setID} FF\r`)
-    }
-    else {
-      logger.silly('Skipping getTileId while tile mode is disabled to avoid errors')
-    }
+  function getAll() {
+    // Retrieve all registered SNMP OIDs and update overture variables
+    let varlist = Object.keys(oids).filter( x => x !== 'IRCommands')
+    let varlist_oids = varlist.map( x => oids[x] )
+    let varlist_string = varlist.reduce( (acc, x) => acc + ', ' + x )
+    logger.debug(`Retrieving SNMP values for ${varlist_string}`)
+    base.commandDefer(CMD_DEFER_TIME)
+    snmpSession.getAll({ oids: varlist_oids }, function (error, varbinds) {
+      if (error) {
+        logger.error(`getAll error: ${error.message}`)
+      }
+      base.commandDone()
+      // logger.silly(`varbinds: ${JSON.stringify(varbinds, null, 2)}`)
+      for (let varbind of varbinds) {
+        let varname = Object.keys(oids).find( x => compareOidArrays(oids[x], varbind.oid) )
+        let value = parseInt(varbind.value, 16)  // Parse hex string into integer value
+        logger.debug(`${varname}: ${varbind.oid} = ${value} (${varbind.value})`)
+        if (varname === 'Sources') {
+          let source = sourcesInfo.find( x => x.hexcode === value )
+          if (source) {
+            value = sourcesInfo.indexOf(source)  // Set value to index of source enum
+          }
+          else {
+            logger.error(`Could not find current source hexcode (${value.toString(16)}) in available sources`)
+            continue  // Go to next varbind
+          }
+        }
+        else if (varname === 'AudioMute') {
+          value = [1, 0][value]  // Mute logic is reversed
+        }
+        base.getVar(varname).value = value
+      }
+    })
   }
 
 
   // ------------------------------ SET FUNCTIONS ------------------------------
 
   function setPower(params) {
-    if (params.Status == 'Off') {
-      sendDefer(`ka ${config.setID} 00\r`)
+    // params: Status
+    if (params.Status == 0) {
+      setSnmpVar('Power', params.Status)
     }
-    else if (params.Status == 'On') {
+    else if (params.Status == 1) {
       wol.wake(config.mac).then(
-        resolved => {
+        () => {
           logger.silly(`setPower: WOL sent to ${config.mac}`)
         },
         error => {
@@ -270,72 +147,106 @@ exports.createDevice = base => {
   }
 
   function selectSource(params) {
-    sendDefer(`xb ${config.setID} ${sourcesInfo.find(x => x.title === params.Name).hexcode.toString(16)}\r`)
+    // params: Name
+    let sourceval = sourcesInfo.find( x => x.title === params.Name ).hexcode
+    setSnmpVar('Sources', sourceval)
   }
 
   function setScreenMute(params) {
-    if (params.Status == 'Off') sendDefer(`kd ${config.setID} 00\r`)
-    else if (params.Status == 'On') sendDefer(`kd ${config.setID} 01\r`)
+    // params: Status
+    setSnmpVar('ScreenMute', params.Status)
   }
 
   function setAudioMute(params) {
-    if (params.Status == 'Off') sendDefer(`ke ${config.setID} 01\r`)
-    else if (params.Status == 'On') sendDefer(`ke ${config.setID} 00\r`)
+    // params: Status
+    setSnmpVar('AudioMute', [1, 0][params.Status])  // Logic is reversed
   }
 
   function setAudioLevel(params) {
-    sendDefer(`kf ${config.setID} ${params.Level.toString(16)}\r`)
+    // params: Level
+    setSnmpVar('AudioLevel', params.Level)
   }
 
   function sendIRCommand(params) {
-    base.getVar('IRCommands').string = params.Name  // Set to command name, and reset to idle in frame parser
-    sendDefer(`mc ${config.setID} ${irCodes[params.Name]}\r`)
-  }
-
-  function setTileMode(params) {
-    let rows, cols
-    let match = params.Status.match(/(\d)x(\d)/)
-    if (match) {
-      rows = match[1]
-      cols = match[2]
-    }
-    else {
-      rows = 0
-      cols = 0
-    }
-    sendDefer(`dd ${config.setID} ${cols.toString(16)}${rows.toString(16)}\r`)
-  }
-
-  function setTileId(params) {
-    let idMax = currentTileIdMax()
-    if (params.Value <= idMax) {
-      sendDefer(`di ${config.setID} ${params.Value.toString(16)}\r`)
-    }
-    else {
-      let mode = base.getVar('TileMode').string
-      logger.error(`Requested Tile ID (${params.Value}) exceeds Tile Mode maximum (${mode} = ${idMax})`)
-    }
+    // params: Name
+    base.getVar('IRCommands').string = params.Name  // Set to command name, and reset to idle in response handler
+    setSnmpVar('IRCommands', irCodes[params.Name])
   }
 
 
   // ------------------------------ HELPER FUNCTIONS ------------------------------
 
-  function currentTileIdMax() {
-    // Make sure the current Tile ID is <= rows x cols, otherwise reset to 1
-    let match = base.getVar('TileMode').string.match(/(\d)x(\d)/)
-    if (match) {
-      return parseInt(match[1]) * parseInt(match[2])
+  function importIrCodes(fname) {
+    // Import IR codes from json as hex strings, convert to object of name: integer values
+    let codes_strings = require(fname)
+    let codes = {}
+    for (let command in codes_strings) {
+      codes[command] = parseInt(codes_strings[command], 16)
+    }
+    return codes
+  }
+
+  function importOids(fname) {
+    // Import from json in string format, and convert to array format for snmp-native module
+    let oids_strings = require(fname)
+    let oids_arrays = {}
+    for (let varname in oids_strings) {
+      oids_arrays[varname] = getOidArray(oids_strings[varname])
+    }
+    return oids_arrays
+  }
+
+  function getOidArray(oidString) {
+    if (typeof oidString === 'string' || oidString instanceof String) {
+      return oidString.match(/\.?\d+/g).map(x => parseInt(x.replace('.','')))
     }
     else {
-      return 1
+      logger.error(`getOidArray requires a string as input, but got ${typeof oidString}: ${oidString}`)
     }
+  }
+
+  function compareOidArrays(oid1, oid2) {
+    if ( !(oid1 instanceof Array) || !(oid2 instanceof Array) ) {
+      throw new Error('compareOidArray only accepts arrays as input')
+    }
+    if (oid1.length !== oid2.length) return false
+    for (let i = 0; i < oid1.length; i++) {
+      if (oid1[i] !== oid2[i]) return false
+    }
+    return true
+  }
+
+  function setSnmpVar(varname, value) {
+    // Type 4 = Octet String
+    snmpSession.set({ oid: oids[varname], value: value.toString(16), type: 4 }, function (error) {
+      if (error) {
+        logger.error(`set ${varname} error: ${error.message}`)
+      } else {
+        if (varname === 'Power' || varname === 'ScreenMute' || varname === 'AudioLevel') {
+          base.getVar(varname).value = value  // Simple integer value
+        }
+        else if (varname === 'AudioMute') {
+          base.getVar('AudioMute').value = [1, 0][value]  // Logic is reversed
+        }
+        else if (varname === 'Sources') {
+          let sourcename = sourcesInfo.find( x => x.hexcode === value ).title
+          base.getVar('Sources').string = sourcename
+        }
+        else if (varname === 'IRCommands') {
+          base.getVar('IRCommands').value = 0  // Set back to idle state
+        }
+        else {
+          logger.error(`setSnmpVar: no logic found for ${varname}`)
+        }
+      }
+    })
   }
 
 
   // ------------------------------ EXPORTED FUNCTIONS ------------------------------
   return {
     setup, start, stop, tick,
-    setPower, selectSource, setScreenMute, setAudioMute, setAudioLevel, sendIRCommand, setTileMode, setTileId,
-    getPower, getSource, getScreenMute, getAudioMute, getAudioLevel, getTemperature, getTileMode, getTileId
+    setPower, selectSource, setScreenMute, setAudioMute, setAudioLevel, sendIRCommand,
+    getAll
   }
 }
--- drivers/lg_display_snmp/readme.md
+++ drivers/lg_display_snmp/readme.md
@@ -1,16 +1,14 @@
-# LG Display
+# LG Display - SNMP
 
 
 ## Overview
-Driver for an LG display, with optional videowall capability.
+Driver for a Commercial LG display using SNMP Protocol
 
 
 ## Setup
 - "Host": The IP address or host name of the device.
-- "Port": The port of TCP communication. (Default 9761)
+- "Port": The port for SNMP communication. (Default 161)
 - "MAC Address": The MAC Address of the device, required for Wake On Lan functionality.
-- "Set ID": Found in General Settings.
-- "Enable Videowall Control": Enabled tile mode controls. Will only work on videowall capable displays.
 - "Available Inputs": The inputs enabled on this display.
 
 
@@ -44,23 +42,6 @@ Driver for an LG display, with optional videowall capability.
 - minimum: 0
 - maximum: 100
 
-### Temperature
-[integer] Temperature of the display.
-
-### Tile Mode (If Enabled)
-[enum] The displays tile mode status
-- "Off": Tile mode disabled
-- "1x2" : 1 row, 2 columns
-- "2x2" : 2 row, 2 columns
-- "3x3" : 3 row, 3 columns
-- "4x4" : 4 row, 4 columns
-- "5x5" : 5 row, 5 columns
-
-### Tile ID (If Enabled)
-[integer] This screens position in videowall. Top left = 1, Bottom right = rows x columns (e.g. 3x3 = 9)
-- minimum: 1
-- maximum: rows x columns
-
 
 ## Commands
 
@@ -84,41 +65,8 @@ Mute or unmute the sound.
 Set Audio level of the device (between 0 and 100)
 - "Level": [integer] audio level for the display.
 
-### Set Tile Mode
-Turn tile mode on or off
-- "Status": [enum] "Off" for disabled, or choose the videowall size
-
-### Set Tile ID
-Set this screens tile ID (position in the videowall, top left = 1)
-- "Value": [integer] Tile ID
-
 
 ## Revisions
 
 ### 1.0.0
 - Initial version, forked from lg_display
-
-### 1.0.1
-- No longer using getMacAddress and persistent variables due to stability issues.
-
-### 1.0.2
-- Added screen mute functionality for faster sleep/wake cycles.
-
-### 1.0.3
-- Removed Brightness and Contrast controls for simplicity.
-- Changed WOL module to a newer version.
-- Increased WOL reliability.
-- Available sources are now selected as part of config.
-
-### 1.0.4
-- Added videowall functionality. Must be enabled in config.
-
-### 1.0.5
-- Added an alternative DVI input source for hex code 0x80
-
-### 1.0.6
-- Added IR remote control commands
-- Changed HDMI/DVI to use PC mode instead of DTV mode
-- On power off, polling is stopped immediately to avoid panel coming back online
-- Swapped row/col in api call, as it was backwards
-- Other minor bug fixes
