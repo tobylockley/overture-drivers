9a920f448df4748d6221b8b8d8e2b624348654a7
Toby Lockley <toby@avdistributors.com>
2019-09-17 16:29:53 +1000

    Cleaned up tl4x1hd2 for customer

--- drivers/techlogix_tl4x1hd2/index.js
+++ drivers/techlogix_tl4x1hd2/index.js
@@ -1,136 +1,140 @@
-'use strict'
+'use strict';
 
-const CMD_DEFER_TIME = 3000        // Timeout when using commandDefer
-const TICK_PERIOD = 5000           // In-built tick interval
-const POLL_PERIOD = 30000          // Continuous polling function interval
-const TCP_TIMEOUT = 35000          // Will timeout after this length of inactivity
-const TCP_RECONNECT_DELAY = 3000   // How long to wait before attempting to reconnect
+const CMD_DEFER_TIME = 3000; // Timeout when using commandDefer
+const TICK_PERIOD = 5000; // In-built tick interval
+const POLL_PERIOD = 20000; // Continuous polling function interval
+const TCP_TIMEOUT = 30000; // Will timeout after this length of inactivity
+const TCP_RECONNECT_DELAY = 3000; // How long to wait before attempting to reconnect
 
-let host
+let host;
 exports.init = _host => {
-  host = _host
-}
+  host = _host;
+};
 
 exports.createDevice = base => {
-  const logger = base.logger || host.logger
-  let config
-  let tcpClient
+  const logger = base.logger || host.logger;
+  let config;
+  let tcpClient;
 
-  let frameParser = host.createFrameParser()
-  frameParser.setSeparator('\r\n')
-  frameParser.on('data', data => onFrame(data))
-
+  let frameParser = host.createFrameParser();
+  frameParser.setSeparator('\r\n');
+  frameParser.on('data', data => onFrame(data));
 
   // ------------------------------ SETUP FUNCTIONS ------------------------------
 
-  function isConnected() { return base.getVar('Status').string === 'Connected' }
+  function isConnected() {
+    return base.getVar('Status').string === 'Connected';
+  }
 
   function setup(_config) {
-    config = _config
-    base.setTickPeriod(TICK_PERIOD)
-    base.setPoll({ action: 'keepAlive', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: false })
+    config = _config;
+    base.setTickPeriod(TICK_PERIOD);
+    base.setPoll({
+      action: 'keepAlive',
+      period: POLL_PERIOD,
+      enablePollFn: isConnected,
+      startImmediately: false
+    });
   }
 
   function start() {
-    initTcpClient()
+    initTcpClient();
   }
 
   function stop() {
-    base.getVar('Status').string = 'Disconnected'
-    tcpClient && tcpClient.end()
-    tcpClient = null
-    base.clearPendingCommands()
-    base.stopPolling()
+    base.getVar('Status').string = 'Disconnected';
+    tcpClient && tcpClient.end();
+    tcpClient = null;
+    base.clearPendingCommands();
+    base.stopPolling();
   }
 
   function tick() {
-    if (!tcpClient) initTcpClient()
+    if (!tcpClient) initTcpClient();
   }
 
   function initTcpClient() {
-    if (tcpClient) return  // Return if tcpClient already exists
+    if (tcpClient) return; // Return if tcpClient already exists
 
-    logger.silly('Initialising TCPClient...')
-    tcpClient = host.createTCPClient()
+    logger.silly('Initialising TCPClient...');
+    tcpClient = host.createTCPClient();
     tcpClient.setOptions({
       receiveTimeout: TCP_TIMEOUT,
-      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY,
+      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY
       // disconnectOnReceiveTimeout: false
-    })
-    tcpClient.connect(config.port, config.host)
+    });
+    tcpClient.connect(config.port, config.host);
 
     tcpClient.on('connect', () => {
-      logger.silly('TCPClient connected')
-      base.getVar('Status').string = 'Connected'
-      base.startPolling()
-    })
+      logger.silly('TCPClient connected');
+      base.getVar('Status').string = 'Connected';
+      base.startPolling();
+    });
 
     tcpClient.on('data', data => {
-      frameParser.push(data.toString())
-    })
+      frameParser.push(data.toString());
+    });
 
     tcpClient.on('close', () => {
-      logger.silly('TCPClient closed')
-      base.getVar('Status').string = 'Disconnected'  // Triggered on timeout, this allows auto reconnect
-    })
+      logger.silly('TCPClient closed');
+      base.getVar('Status').string = 'Disconnected'; // Triggered on timeout, this allows auto reconnect
+    });
 
     tcpClient.on('error', error => {
-      logger.error(`TCPClient error: ${error.message}`)
-      stop()  // Throw out the tcpClient and get a fresh connection
-    })
+      logger.error(`TCPClient error: ${error.message}`);
+      stop(); // Throw out the tcpClient and get a fresh connection
+    });
   }
-
 
   // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
 
   function send(data) {
-    logger.silly(`TCPClient send: ${data}`)
-    return tcpClient && tcpClient.write(data)
+    logger.silly(`TCPClient send: ${data}`);
+    return tcpClient && tcpClient.write(data);
   }
 
   function sendDefer(data) {
-    base.commandDefer(CMD_DEFER_TIME)
+    base.commandDefer(CMD_DEFER_TIME);
     if (!send(data)) {
-      base.commandError('Data not sent')
+      base.commandError('Data not sent');
     }
   }
 
   function onFrame(data) {
     if (data.trim().length === 0) {
-      logger.debug('onFrame data is empty string, skipping...')
-      return
+      logger.debug('onFrame data is empty string, skipping...');
+      return;
     }
 
-    let pendingCommand = base.getPendingCommand()
-    logger.debug(`onFrame (pending = ${pendingCommand && pendingCommand.action}): ${data.trim()}`)
+    let pendingCommand = base.getPendingCommand();
+    logger.debug(
+      `onFrame (pending = ${pendingCommand &&
+        pendingCommand.action}): ${data.trim()}`
+    );
     if (pendingCommand) {
-      let pending = pendingCommand.action
+      let pending = pendingCommand.action;
       // Feedback Example - [CMD]: switch to HDMI1.
-      let match = data.match(/switch to (HDMI\d)/)
-      if ( pending === 'selectSource' && (match = data.match(/switch to (HDMI\d)/)) ) {
-        base.getVar('Sources').string = match[1]
-        base.commandDone()
-      }
-      else if ( pending === 'keepAlive' && /CMD NOT FOUND/.test(data) ) {
-        base.commandDone()
-      }
-      else {
-        logger.warn('onFrame data not processed')
+      let match = data.match(/switch to (HDMI\d)/);
+      if (match && pending === 'selectSource') {
+        base.getVar('Sources').string = match[1];
+        base.commandDone();
+      } else if (pending) {
+        base.commandDone();
+      } else {
+        logger.warn('onFrame data not processed');
       }
     }
   }
-
 
   // ------------------------------ SET FUNCTIONS ------------------------------
 
   function keepAlive() {
-    sendDefer('%')  // Will cause an error, this is just to test connection and keep socket alive
+    sendDefer('%'); // Will cause an error, this is just to test connection and keep socket alive
   }
 
   function selectSource(params) {
-    sendDefer(`${params.Name}%`)
+    sendDefer(`${params.Name}%`);
   }
-
 
   // ------------------------------ EXPORTED FUNCTIONS ------------------------------
   return {
@@ -140,5 +144,5 @@ exports.createDevice = base => {
     tick,
     keepAlive,
     selectSource
-  }
-}
+  };
+};
