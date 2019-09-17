49a24d3801918a95f23d2675d83eeeed64291ac9
Toby Lockley <toby@avdistributors.com>
2019-09-17 16:30:50 +1000

    Updated various IR drivers

--- drivers/teac_hdb850_new/index.js
+++ drivers/teac_hdb850_new/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const CMD_DEFER_TIME = 1000;        // Timeout when using commandDefer
-const TICK_PERIOD = 5000;           // In-built tick interval
-const POLL_PERIOD = 30000;           // Continuous polling function interval
-const TCP_TIMEOUT = 30000;          // Will timeout after this length of inactivity
-const TCP_RECONNECT_DELAY = 3000;   // How long to wait before attempting to reconnect
+const CMD_DEFER_TIME = 3000; // Timeout when using commandDefer
+const TICK_PERIOD = 5000; // In-built tick interval
+const POLL_PERIOD = 20000; // Continuous polling interval used for keepAlive
+const TCP_TIMEOUT = 30000; // Will timeout after this length of inactivity
+const TCP_RECONNECT_DELAY = 3000; // How long to wait before attempting to reconnect
 
 let host;
 exports.init = _host => {
@@ -15,53 +15,46 @@ exports.createDevice = base => {
   const logger = base.logger || host.logger;
   let config;
   let tcpClient;
+  const ir_codes = require('./ir_codes.json');
 
-  function isConnected() { return base.getVar('Status').string === 'Connected'; }
+  // ------------------------------ SETUP FUNCTIONS ------------------------------
+
+  function isConnected() {
+    return base.getVar('Status').string === 'Connected';
+  }
 
   function setup(_config) {
     config = _config;
     base.setTickPeriod(TICK_PERIOD);
-    base.setPoll({ action: 'keepAlive', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true });
+
+    // Register polling functions
+    base.setPoll({
+      action: 'keepAlive',
+      period: POLL_PERIOD,
+      enablePollFn: isConnected,
+      startImmediately: true
+    });
+
+    base.getVar('IR_Commands').enums = ['Idle'].concat(Object.keys(ir_codes));
   }
 
   function start() {
     initTcpClient();
   }
 
-  function tick() {
-    if (!tcpClient) initTcpClient();
-  }
-
   function stop() {
+    base.clearPendingCommands();
     base.getVar('Status').string = 'Disconnected';
     tcpClient && tcpClient.end();
     tcpClient = null;
   }
 
-  let ir_commands = require('./ir_codes.js');
-
-  function sendCommand(params) {
-    // base.getVar("IRCommands").string = params.Name;
-    let ircode = ir_commands[params.Name.replace(/\s/g, '_').toUpperCase()];
-    if (ircode) {
-      sendDefer(`sendir,${config.module}:${config.ir_port},${ircode}\r`);
-    }
-    else {
-      logger.debug('Wrong command variable sent to function Send Command()');
-    }
-  }
-
-  function keepAlive() {
-    sendDefer('A\r');
-  }
-
-  function onFrame(data) {
-    logger.silly(`onFrame ${data}`);
-    base.commandDone();
+  function tick() {
+    if (!tcpClient) initTcpClient();
   }
 
   function initTcpClient() {
-    if (tcpClient) return;  // Return if tcpClient already exists
+    if (tcpClient) return; // Return if tcpClient already exists
 
     tcpClient = host.createTCPClient();
     tcpClient.setOptions({
@@ -77,35 +70,77 @@ exports.createDevice = base => {
     });
 
     tcpClient.on('data', data => {
-      onFrame(data);
+      onFrame(data.toString());
     });
 
     tcpClient.on('close', () => {
       logger.silly('TCPClient closed');
-      base.getVar('Status').string = 'Disconnected';  // Triggered on timeout, this allows auto reconnect
+      base.getVar('Status').string = 'Disconnected'; // Triggered on timeout, this allows auto reconnect
     });
 
     tcpClient.on('error', err => {
       logger.error(`TCPClient: ${err}`);
-      stop();  // Throw out the tcpClient and get a fresh connection
+      stop(); // Throw out the tcpClient and get a fresh connection
     });
   }
 
-  function sendDefer(data) {
-    if (send(data)) {
-      base.commandDefer(CMD_DEFER_TIME);
-    } else {
-      base.commandError('Data not sent');
-    }
-  }
+  // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
 
   function send(data) {
     logger.silly(`TCPClient send: ${data}`);
     return tcpClient && tcpClient.write(data);
   }
 
+  function sendDefer(data) {
+    base.commandDefer(CMD_DEFER_TIME);
+    if (!send(data)) base.commandError('Data not sent');
+  }
+
+  function onFrame(data) {
+    const pendingCommand = base.getPendingCommand();
+    if (pendingCommand && pendingCommand.action === 'sendCommand') {
+      const cmdName = base.getVar('IR_Commands').enums[
+        pendingCommand.params.Index
+      ];
+      logger.silly(`pendingCommand: ${cmdName}  -  received: ${data}`);
+      base.getVar('IR_Commands').value = 0; // Set back to idle
+      base.commandDone();
+    }
+    if (pendingCommand && pendingCommand.action === 'keepAlive') {
+      base.commandDone();
+    }
+  }
+
+  // ------------------------------ DEVICE FUNCTIONS ------------------------------
+
+  function sendCommand(params) {
+    let name = base.getVar('IR_Commands').enums[params.Index];
+    let code = ir_codes[name];
+    if (code) {
+      base.getVar('IR_Commands').value = params.Index;
+      sendDefer(`sendir,${config.module}:${config.ir_port},${code}\r`);
+      setTimeout(function() {
+        let v = base.getVar('IR_Commands');
+        if (v.value !== 0) v.value = 0; // Set back to idle
+      }, 1);
+    } else {
+      logger.error(
+        `Invalid command index sent to function sendCommand: ${params.Index}`
+      );
+    }
+  }
+
+  function keepAlive() {
+    sendDefer('getversion\r');
+  }
+
+  // ------------------------------ EXPORTED FUNCTIONS ------------------------------
   return {
-    setup, start, stop, tick,
-    keepAlive, sendCommand
+    setup,
+    start,
+    stop,
+    tick,
+    sendCommand,
+    keepAlive
   };
 };
