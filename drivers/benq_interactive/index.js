d2059f3e246b8ffe32bddb814040619fdfad9c41
Toby Lockley <toby@avdistributors.com>
2019-09-17 16:31:27 +1000

    Started benq interactive panel driver

---
+++ drivers/benq_interactive/index.js
@@ -0,0 +1,304 @@
+const CMD_DEFER_TIME = 30000; // Timeout when using commandDefer
+const TICK_PERIOD = 5000; // In-built tick interval
+const TCP_TIMEOUT = 10000; // Will timeout after this length of inactivity
+const TCP_RECONNECT_DELAY = 3000; // How long to wait before attempting to reconnect
+
+let host;
+exports.init = _host => {
+  host = _host;
+};
+
+exports.createDevice = base => {
+  const logger = base.logger || host.logger;
+  let config;
+  let tcpClient;
+
+  let frameParser = host.createFrameParser();
+  frameParser.setSeparator('\r');
+  frameParser.on('data', data => onFrame(data));
+
+  //------------------------------------------------------------------------- STANDARD SDK FUNCTIONS
+  function setup(_config) {
+    config = _config;
+    base.setTickPeriod(TICK_PERIOD);
+
+    // Register polling functions
+    let poll_ms = config.polltime * 1000; // Convert from seconds to milliseconds
+    setPoll('getPower', poll_ms, isConnected);
+    setPoll('getSource', poll_ms, isPoweredOn);
+    setPoll('getAudioLevel', poll_ms, isPoweredOn);
+    setPoll('getAudioMute', poll_ms, isPoweredOn);
+  }
+
+  function start() {
+    if (config.simulation) base.getVar('Status').string = 'Connected';
+    else initTcpClient();
+  }
+
+  function tick() {
+    if (!config.simulation && !tcpClient) initTcpClient();
+  }
+
+  function disconnect() {
+    base.getVar('Status').string = 'Disconnected';
+    base.stopPolling();
+  }
+
+  function stop() {
+    disconnect();
+    tcpClient && tcpClient.end();
+    tcpClient = null;
+  }
+
+  function initTcpClient() {
+    if (tcpClient) return; // Return if tcpClient already exists
+
+    tcpClient = host.createTCPClient();
+    tcpClient.setOptions({
+      receiveTimeout: TCP_TIMEOUT,
+      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY
+    });
+    tcpClient.connect(config.port, config.host);
+
+    tcpClient.on('connect', () => {
+      logger.silly('TCPClient connected');
+      base.getVar('Status').string = 'Connected';
+      base.startPolling();
+    });
+
+    tcpClient.on('data', data => {
+      frameParser.push(data.toString());
+    });
+
+    tcpClient.on('close', () => {
+      logger.silly('TCPClient closed');
+      disconnect(); // Triggered on timeout, this allows auto reconnect
+
+      let pending = base.getPendingCommand();
+      if (pending) {
+        base.commandError('Lost Connection');
+        base.perform(pending.action, pending.params);
+      }
+    });
+
+    tcpClient.on('error', err => {
+      logger.error(`TCPClient: ${err}`);
+      stop(); // Throw out the tcpClient and get a fresh connection
+    });
+  }
+
+  //-------------------------------------------------------------------------- SEND/RECEIVE HANDLERS
+  function send(data) {
+    logger.silly(`TCPClient send: ${data}`);
+    return tcpClient && tcpClient.write(data);
+  }
+
+  function sendDefer(data) {
+    base.commandDefer(CMD_DEFER_TIME);
+    if (!send(data)) {
+      base.commandError('Data not sent');
+    }
+  }
+
+  function onFrame(data) {
+    let match; // Used for regex matching below
+    const pendingCommand = base.getPendingCommand();
+
+    logger.silly(
+      `onFrame (pending = ${pendingCommand && pendingCommand.action}): ${data}`
+    );
+
+    // Use arrays to match pending command action to expected response
+    const setFns = [
+      'setPower',
+      'selectSource',
+      'setAudioLevel',
+      'setAudioMute',
+      'setChannel',
+      'shiftChannel'
+    ];
+
+    if (pendingCommand && setFns.includes(pendingCommand.action)) {
+      // Parse response after issueing a SET function
+
+      match = data.match(/POWR0{16}/);
+      if (match) {
+        base.getVar('Power').string = pendingCommand.params.Status;
+        base.commandDone();
+      }
+
+      match = data.match(/INPT0{16}/);
+      if (match) {
+        base.getVar('Sources').string = pendingCommand.params.Name;
+        base.commandDone();
+        if (pendingCommand.params.Name === 'DTV') getChannel(); // Get channel when set to DTV mode
+      }
+
+      match = data.match(/VOLU0{16}/);
+      if (match) {
+        base.getVar('AudioLevel').value = parseInt(pendingCommand.params.Level);
+        base.commandDone();
+      }
+
+      match = data.match(/AMUT0{16}/);
+      if (match) {
+        base.getVar('AudioMute').string = pendingCommand.params.Status;
+        base.commandDone();
+      }
+
+      match = data.match(/CHNN0{16}/);
+      if (match) {
+        base.getVar('Channel').value = parseInt(pendingCommand.params.Name);
+        base.commandDone();
+      }
+
+      match = data.match(/IRCC0{16}/);
+      if (match) {
+        base.getVar('ChannelShift').value = 0; // Reset to 'idle'
+        base.commandDone();
+      }
+    } else {
+      // Parse response after issueing a GET function, OR after a "notify" frame
+      // Notify frames are received after changing something, either from the CS or an IR remote
+
+      match = data.match(/POWR(.{16})/);
+      if (match) {
+        let val = parseInt(match[1]);
+        val && (base.getVar('Power').value = val); // 0 = off, 1 = on
+        pendingCommand && base.commandDone();
+      }
+
+      match = data.match(/INPT0{16}/);
+      if (match) {
+        base.getVar('Sources').string = 'DTV';
+        pendingCommand && base.commandDone();
+      }
+
+      match = data.match(/INPT0{7}1(\d+)/);
+      if (match) {
+        base.getVar('Sources').string = `HDMI${parseInt(match[1])}`;
+        pendingCommand && base.commandDone();
+      }
+
+      match = data.match(/VOLU(.{16})/);
+      if (match) {
+        let val = parseInt(match[1]);
+        val && (base.getVar('AudioLevel').value = val);
+        pendingCommand && base.commandDone();
+      }
+
+      match = data.match(/AMUT(.{16})/);
+      if (match) {
+        let val = parseInt(match[1]);
+        val && (base.getVar('AudioMute').value = val); // 0 = unmute, 1 = mute
+        pendingCommand && base.commandDone();
+      }
+
+      match = data.match(/CHNN(\d+)\./); // Ignores values after decimal point
+      if (match) {
+        base.getVar('Channel').value = parseInt(match[1]);
+        pendingCommand && base.commandDone();
+      }
+    }
+  }
+
+  //---------------------------------------------------------------------------------- GET FUNCTIONS
+  function getPower() {
+    sendDefer('*SEPOWR################\n');
+  }
+
+  function getSource() {
+    sendDefer('*SEINPT################\n');
+  }
+
+  function getAudioLevel() {
+    sendDefer('*SEVOLU################\n');
+  }
+
+  function getAudioMute() {
+    sendDefer('*SEAMUT################\n');
+  }
+
+  function getChannel() {
+    sendDefer('*SECHNN################\n');
+  }
+
+  //---------------------------------------------------------------------------------- SET FUNCTIONS
+  function setPower(params) {
+    if (config.simulation) {
+      base.getVar('Power').string = params.Status;
+      return;
+    }
+
+    if (params.Status == 'Off') sendDefer('*SCPOWR0000000000000000\n');
+    else if (params.Status == 'On') sendDefer('*SCPOWR0000000000000001\n');
+  }
+
+  function selectSource(params) {
+    if (config.simulation) {
+      base.getVar('Sources').string = params.Name;
+      return;
+    }
+
+    if (params.Name == 'DTV') sendDefer('*SCINPT0000000000000000\n');
+    else {
+      let match = params.Name.match(/HDMI(\d)/);
+      match && sendDefer(`*SCINPT000000010000000${match[1]}\n`);
+    }
+  }
+
+  function setAudioLevel(params) {
+    if (config.simulation) {
+      base.getVar('AudioLevel').value = params.Level;
+      return;
+    }
+
+    let vol = params.Level.toString().padStart(3, '0'); // Formats the integer with leading zeroes, e.g. 53 = '053'
+    sendDefer(`*SCVOLU0000000000000${vol}\n`);
+  }
+
+  function setAudioMute(params) {
+    if (config.simulation) {
+      base.getVar('AudioMute').string = params.Status;
+      return;
+    }
+
+    if (params.Status == 'Off') sendDefer('*SCAMUT0000000000000000\n');
+    else if (params.Status == 'On') sendDefer('*SCAMUT0000000000000001\n');
+  }
+
+  //------------------------------------------------------------------------------- HELPER FUNCTIONS
+  function isConnected() {
+    return base.getVar('Status').string === 'Connected';
+  }
+
+  function isPoweredOn() {
+    return isConnected() && base.getVar('Power').string === 'On';
+  }
+
+  function setPoll(action, period, enableFn) {
+    base.setPoll({
+      action: action,
+      period: period,
+      enablePollFn: enableFn,
+      startImmediately: true
+    });
+  }
+
+  //----------------------------------------------------------------------------- EXPORTED FUNCTIONS
+  return {
+    setup,
+    start,
+    stop,
+    tick,
+    getPower,
+    getSource,
+    getAudioLevel,
+    getAudioMute,
+    getChannel,
+    setPower,
+    selectSource,
+    setAudioLevel,
+    setAudioMute
+  };
+};
---
+++ drivers/benq_interactive/readme.md
@@ -0,0 +1,118 @@
+# Sony Bravia
+
+## Overview
+
+Driver for a Sony Bravia device over a TCP connection.
+
+## Setup
+
+  - "Host": The IP address or host name of the device. (Default 192.168.3.20)
+  - "Port": The port of TCP communication. (Default 20060)
+
+## Variables
+
+### Status
+
+[enum] The current connection status of the device.
+  - "Disconnected" : The device is not connected.
+  - "Connected" : The device is connected, and able to receive commands.
+
+### Power
+
+[enum] The power status of the display.
+  - "Off": The display is off.
+  - "On" : The display is on.
+
+### Sources
+
+[enum] List of input sources user can select from
+  - "DTV": Change input source to DTV
+  - "HDMI1": Change input source to HDMI1
+  - "HDMI2": Change input source to HDMI2
+  - "HDMI3": Change input source to HDMI3
+  - "HDMI4": Change input source to HDMI4
+
+### Audio Level
+
+[integer] Set the audio level of the device.
+  - minimum: 0
+  - maximum: 100
+
+### Audio Mute
+
+[enum] The mute status of the device.
+  - "Off": The device is unmuted.
+  - "On" : The device is muted.
+
+### Channel
+
+[integer] Set the channel of the device.
+  - minimum: 1
+  - maximum: 99
+
+### Channel Shift
+
+[enum] Shift the channel of the device up or down.
+  - "": First val is null, for UI purposes
+  - "Up": Change channel up.
+  - "Down" : Change channel down.
+
+## Commands
+
+### Set Power
+Turn on or off the device power.
+  - "Status": [enum] "On" or "Off" to turn power on or off.
+
+### Select Source
+To select the input source to display.
+  - "Name": [string] the name of the input source (given by variable "Sources")
+
+### Set Audio Level
+Set Audio level of the device (between 0 and 100)
+  - "Level": [integer] audio level for the display.
+
+### Set Audio Mute
+Mute or unmute the device.
+  - "Status": [enum] "On" or "Off" to mute or unmute the display.
+
+### Set Channel
+Only available in DTV mode. Set the DTV channel of the display.
+  - "Name": [integer] channel for the display.
+
+### Shift Channel
+Only available in DTV mode. Shift the DTV channel of the display up or down.
+  - "Name": [enum] "Up" or "Down" to change channel accordingly.
+
+## Revisions
+
+### 1.0.0
+  - Initial version
+
+### 1.0.1
+  - Added 'Shift Channel' variable to allow UI controls to be added.
+
+### 1.0.2
+  - Added 'Simulation Mode' to aid with UI development.
+  - Updated the onFrame response handler.
+  - Changed polling for values to be only when device is on.
+
+### 1.0.3
+  - Added ability to rename sources during setup.
+
+### 1.0.4
+  - Bug fixes for source nicknames.
+
+### 1.0.5
+  - Improved "Channe Shift" response speed.
+  - Firing "getChannel" when put in DTV mode to increase UI response speed.
+  - Updated onFrame to also accept "notify" frames.
+  - Fixed a bug that prevented polling functions from firing immediately on connection.
+
+### 1.0.6
+  - Removed nickname feature. Can use overtures in-built enum labels instead.
+
+### 1.0.7
+  - Clear pending commands when driver stops or loses connection.
+
+### 1.0.8
+  - Attempting to hold a queue of commands for when device goes offline momentarily
