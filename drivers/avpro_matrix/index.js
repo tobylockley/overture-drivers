c8c2d1f282895ccf02ff7aab341551fbc0602273
Toby Lockley <toby@avdistributors.com>
2019-10-15 16:08:41 +1000

    Removed nicknames, fixed selectSource bug, more robust frame parser

--- drivers/avpro_matrix/index.js
+++ drivers/avpro_matrix/index.js
@@ -2,80 +2,58 @@
 
 const CMD_DEFER_TIME = 1000        // Timeout when using commandDefer
 const TICK_PERIOD = 5000           // In-built tick interval
-const POLL_PERIOD = 10000           // Continuous polling function interval
+const POLL_PERIOD = 10000          // Continuous polling function interval
 const TCP_TIMEOUT = 30000          // Will timeout after this length of inactivity
 const TCP_RECONNECT_DELAY = 3000   // How long to wait before attempting to reconnect
-
 
 let host
 exports.init = _host => {
   host = _host
 }
 
-
 exports.createDevice = base => {
   const logger = base.logger || host.logger
   let config
   let tcpClient
+  let getAllTally = 0  // Used for counting getAllOutputs response frames
 
   let frameParser = host.createFrameParser()
   frameParser.setSeparator('\r\n')
-  frameParser.on('data', data => onFrame(data))
+  frameParser.on('data', onFrame)
 
-
-  // ------------------------------ SETUP FUNCTIONS ------------------------------
-
-  function isConnected() { return base.getVar('Status').string === 'Connected' }
-
+  //------------------------------------------------------------------------- BASE FUNCTIONS
   function setup(_config) {
     config = _config
     base.setTickPeriod(TICK_PERIOD)
-    base.setPoll({ action: 'Get All Outputs', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
-
-    // First, build enum list of input names
-    let input_list = []
-    for (let input in config.model.input_names) {
-      let input_name = config.model.input_names[input]
-      // Check for duplicates
-      if (input_list.includes(input_name)) {
-        logger.error(`setup(): WARNING! Duplicate input name (${input_name}). Reverting to default (Input${input})`)
-        input_name = `Input${input}`
-      }
-      if (input_name == '') input_name = `Input${input}`  // If blank, use default 'InputX'
-      input_list.push(input_name)
-
-      // Finally, update the input name in the config data for reference later
-      config.model.input_names[input] = input_name
-    }
-
-    // Now create enum variable for each output, checking for duplicates
-    let output_list = []
-    for (let output in config.model.output_names) {
-      let output_name = config.model.output_names[output]
-      // Check for duplicates
-      if (output_list.includes(output_name)) {
-        logger.error(`setup(): WARNING! Duplicate output name (${output_name}). Reverting to default (Output${output})`)
-        output_name = `Output${output}`
-      }
-      if (output_name == '') output_name = `Output${output}`  // If blank, use default 'OutputX'
-      output_list.push(output_name)
-
-      // Create enum variable for this output
-      base.createVariable({
-        name: `Sources_${output_name}`,
-        type: 'enum',
-        enums: input_list,
-        perform: {
-          action: 'Set Output',
-          params: {
-            Channel: parseInt(output),
-            Name: '$string'
+    base.setPoll({
+      action: 'getAllOutputs',
+      period: POLL_PERIOD,
+      enablePollFn: isConnected,
+      startImmediately: true
+    })
+    // Create an enum variable for each output, each containing enum of inputs
+    let match = config.model.toString().match(/AC-MX(\d)(\d)/)
+    if (match) {
+      config.num_inputs = parseInt(match[1])
+      config.num_outputs = parseInt(match[2])
+      let enums = Array.from(Array(config.num_inputs).keys(), x => `IN${x+1}`)
+      for (let i = 1; i <= config.num_outputs; i++) {
+        base.createVariable({
+          name: `Sources_OUT${i}`,
+          type: 'enum',
+          enums: enums,
+          perform: {
+            action: 'Select Source',
+            params: {
+              Channel: i,
+              Name: '$string'
+            }
           }
-        }
-      })
-
-      // Finally, update the output name in the config data for reference later
-      config.model.output_names[output] = output_name
+        })
+      }
+    }
+    else {
+      logger.error(`Unexpected model: ${config.model}`)
     }
   }
 
@@ -84,16 +62,17 @@ exports.createDevice = base => {
     else initTcpClient()
   }
 
+  function tick() {
+    if (!config.simulation && !tcpClient) initTcpClient()
+  }
+
   function stop() {
     base.getVar('Status').string = 'Disconnected'
     tcpClient && tcpClient.end()
     tcpClient = null
   }
 
-  function tick() {
-    if (!config.simulation && !tcpClient) initTcpClient()
-  }
-
+  //-------------------------------------------------------------------------- SEND/RECEIVE HANDLERS
   function initTcpClient() {
     if (tcpClient) return  // Return if tcpClient already exists
 
@@ -125,79 +104,56 @@ exports.createDevice = base => {
     })
   }
 
-
-  // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
-
   function send(data) {
     logger.silly(`TCPClient send: ${data}`)
     return tcpClient && tcpClient.write(data)
   }
 
   function sendDefer(data) {
-    if (send(data)) base.commandDefer(CMD_DEFER_TIME)
-    else base.commandError('Data not sent')
+    base.commandDefer(CMD_DEFER_TIME)
+    if (!send(data)) base.commandError('Data not sent')
   }
 
+  //---------------------------------------------------------------------------------- FRAME PARSER
   function onFrame(data) {
     let match  // Used for regex matching below
-    logger.silly(`onFrame: ${data}`)
-
-    match = data.match(/OUT(\d).*IN(\d)/i)
+    const pending = base.getPendingCommand()
+    logger.debug(`onFrame (pending = ${pending && pending.action}): ${data}`)
+    match = data.match(/(OUT\d) VS (IN\d)/i)
     if (match) {
-      // Retrieve input and output names
-      let input_name = config.model.input_names[match[2]]
-      let output_name = config.model.output_names[match[1]]
-      base.getVar(`Sources_${output_name}`).string = input_name
-      base.commandDone()
+      base.getVar(`Sources_${match[1]}`).string = match[2]
+      if (pending && pending.action === 'getAllOutputs') {
+        getAllTally += 1
+        if (getAllTally === config.num_outputs) base.commandDone()
+      }
+      else if (pending && pending.action === 'selectSource') {
+        base.commandDone()
+      }
     }
   }
 
-
-  // ------------------------------ GET FUNCTIONS ------------------------------
-
+  //---------------------------------------------------------------------------------- GET FUNCTIONS
   function getAllOutputs() {
+    getAllTally = 0
     sendDefer('GET OUT0 VS\r\n')  // Get all outputs
   }
 
-
-  // ------------------------------ SET FUNCTIONS ------------------------------
-
+  //---------------------------------------------------------------------------------- SET FUNCTIONS
   function selectSource(params) {
-    let output_name = config.model.output_names[params.Channel]
-
-    // Make sure params.Channel is in valid range
-    if (params.Channel < 1 || params.Channel > config.model.output_names.length) {
-      logger.error(`selectSource: params.Channel (${params.Channel}) is out of valid range (1-${config.model.output_names.length})`)
-      return
-    }
-
     // If simulation mode, just set the variable
     if (config.simulation) {
-      base.getVar(`Sources_${output_name}`).string = params.Name
+      base.getVar(`Sources_OUT${params.Channel}`).string = params.Name
       return
     }
-
-    // Find input number based on name
-    let input_number = 0
-    for (let input in config.model.input_names) {
-      if (params.Name == config.model.input_names[input]) {
-        input_number = parseInt(input)
-      }
-    }
-
-    // Send join command, or error message if input not found
-    if (input_number > 0) {
-      logger.debug(`Connecting "${params.Name}" (Input${input_number}) to "${output_name}" (Output${params.Channel})`)
-      sendDefer(`SET OUT${params.Channel} VS IN${input_number}\r\n`)
-    }
-    else {
-      logger.error(`selectSource: Could not find an input matching "${params.Name}"`)
-    }
+    sendDefer(`SET OUT${params.Channel} VS ${params.Name}\r\n`)
   }
 
+  //------------------------------------------------------------------------------- HELPER FUNCTIONS
+  function isConnected() {
+    return base.getVar('Status').string === 'Connected'
+  }
 
-  // ------------------------------ EXPORTED FUNCTIONS ------------------------------
-
+  //----------------------------------------------------------------------------- EXPORTED FUNCTIONS
   return {
     setup, start, stop, tick,
     getAllOutputs, selectSource
--- drivers/avpro_matrix/readme.md
+++ drivers/avpro_matrix/readme.md
@@ -1,27 +1,28 @@
 # AV Pro Connect Matrix Switcher Driver
 
 ## Overview
-
-Driver for an AV Pro Connect NxN switcher (E.g. MX44)
+Driver for an AV Pro Connect matrix switcher. Current models supported:
+- AC-MX42-AUHD
+- AC-MX44-AUHD
+- AC-MX44-AUHD-HDBT
+- AC-MX88-UHD
+- AC-MX88-AUHD-GEN2
+- AC-MX88-AUHD-HDBT
 
 ## Setup
-
-- "Host": The IP address or host name of the GlobalCache device. (Default 192.168.10.120)
-- "Port": The port of TCP communication. (Default 21)
-- "Model": The model being installed.
-- "Nicknames": Optional, can assign nicknames to inputs and/or outputs
+- "Host": The IP address or host name of the GlobalCache device. (Default 192.168.1.239)
+- "Port": The port of TCP communication. (Default 23)
+- "Model": The model being installed. This will dictate number of inputs/outputs.
 
 ## Variables
 
 ### Status
-
 [enum] The current connection status of the device.
-- "Disconnected" : The device is not connected.
-- "Connected" : The device is connected, and able to receive commands.
+- "Disconnected": The device is not connected.
+- "Connected": The device is connected, and able to receive commands.
 
-### Sources_Output{N}
-
-[enum] Which input is being pushed to this output.
+### Sources_OUT{N}
+[enum] Which input is being switched to this output.
 
 ## Commands
 
@@ -36,17 +37,18 @@ Returns all current input -> output assignments. Used when polling.
 ## Revisions
 
 ### 1.0.0-beta1
-
 - Initial version, testing.
 - Allows dynamic variable creation on setup for different models.
 
 ### 1.0.0
-
 - Added polling function (GetAllOutputs) at 10 sec intervals.
 
 ### 1.0.1
-
 - Changed json setup schema to allow input/output nicknames
 - Refactored code to update TCP timeout/reconnect handling
 - Changed setup and variable logic to allow nicknames to function
 - Added option for simulation mode
+
+### 1.0.2
+- Fixed bug where function name for setting outputs was wrong.
+- Removed nicknames to keep it simple.
