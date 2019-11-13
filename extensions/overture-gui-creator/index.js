7110d8616ec342d1131fab32310025565148d9f3
Toby Lockley <toby@avdistributors.com>
2019-11-13 12:43:50 +1000

    Messing with socket api

--- extensions/overture-gui-creator/index.js
+++ extensions/overture-gui-creator/index.js
@@ -1,21 +1,23 @@
-let socket = require('socket.io-client')('http://localhost:8000/overtureapi');
+const UX_URL = 'http://192.168.99.193'
+let socket = require('socket.io-client')(`${UX_URL}/overtureapi`)
 
 socket.on('connect', function() {
-  console.log('connected');
-  runTests();
-});
+  console.log('connected')
+  runTests()
+})
 
 socket.on('disconnect', function() {
-  console.log('socket disconnect');
-});
+  console.log('socket disconnect')
+})
 
 socket.on('overtureapi', function(data) {
-  data.sequence == 123 && console.timeEnd('getpoints');
-  console.log('-------------- OVERTUREAPI --------------');
-  console.log(data);
-});
+  data.sequence == 123 && console.timeEnd('getpoints')
+  console.log('-------------- OVERTUREAPI --------------')
+  console.log(data)
+  if (data.url == '/livepoints/connect' && data.statuscode == 200) doExtras()
+})
 
-console.log('GUI Creator');
+console.log('GUI Creator')
 
 async function runTests() {
   // await delay(2000);
@@ -24,9 +26,9 @@ async function runTests() {
     url: '/livepoints/connect',
     body: {
       token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXljbG9ha0lkIjoiODRjMzBmODctNDkxMy00ZDI3LTlhYjEtM2E1NWFiYzY3NDQzIiwiaWQiOiI1N2I3NTkxYWI0MWY1OTcwOGY3YTFiMTEiLCJuYW1lIjoibWVkaWFsb24iLCJyb2xlcyI6W3siaWQiOiJzeXN0ZW0iLCJuYW1lIjoiU3lzdGVtIiwiYWx0bmFtZSI6InN5c3RlbSIsInN5c3RlbSI6dHJ1ZSwiaHJjX2FjY2Vzc19yaWdodHMiOjEwMCwiaHJjX2FsYXJtc19yaWdodHMiOjEwMCwicm9sZV9pZCI6InN5c3RlbSJ9XSwidXNlcm5hbWUiOiJtZWRpYWxvbiIsImlzTGRhcCI6ZmFsc2V9.UGnaitRFRRM_uD_1tXgXIxJXwzDKTn8uukTuUvi8Ies'
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXljbG9ha0lkIjoiZmM1OTdiZjYtMDgzNi00NmU1LWIyZWUtMWVkYWY1ZmM0MmIzIiwiaWQiOiI1YmEyZTBmMzNiOWQ2NzAwMGVkMDk1ZTQiLCJuYW1lIjoiVG9ieSBMb2NrbGV5Iiwicm9sZXMiOlt7ImlkIjoic3lzdGVtIiwibmFtZSI6IlN5c3RlbSIsImFsdG5hbWUiOiJzeXN0ZW0iLCJzeXN0ZW0iOnRydWUsImhyY19hY2Nlc3NfcmlnaHRzIjoxMDAsImhyY19hbGFybXNfcmlnaHRzIjoxMDAsInJvbGVfaWQiOiJzeXN0ZW0ifV0sInVzZXJuYW1lIjoidG9ieSIsImlzTGRhcCI6ZmFsc2V9.P52DYFUuF-XmyADUAVthNOhoIAaeBAL8YrY60ys8H8w'
     }
-  });
+  })
 
   // await delay(2000);
   // socket.emit('overtureapi', {
@@ -35,25 +37,34 @@ async function runTests() {
   //   body: {}
   // });
 
-  await delay(2000);
-  socket.emit('overtureapi', {
-    verb: 'CMD',
-    url: '/livepoints/subscribe',
-    body: {
-      points: [
-        '5b6288c3a7c7f9000e30515b' // tobys room right sony sources
-      ]
-    }
-  });
+  // await delay(2000)
+  // socket.emit('overtureapi', {
+  //   verb: 'CMD',
+  //   url: '/livepoints/subscribe',
+  //   body: {
+  //     points: [
+  //       '5b6288c3a7c7f9000e30515b' // tobys room right sony sources
+  //     ]
+  //   }
+  // })
 
-  // await delay(2000);
-  // console.time('getpoints');
+  // await delay(2000)
+  // console.time('getpoints')
   // socket.emit('overtureapi', {
   //   verb: 'GET',
   //   url: '/points',
   //   sequence: 123,
   //   body: {}
-  // });
+  // })
+}
+
+
+function doExtras() {
+  socket.emit('overtureapi', {
+    verb: 'GET',
+    url: '/license',
+    body: {}
+  })
 }
 
 function apiCall(verb, url, body, callback) {
@@ -62,6 +73,6 @@ function apiCall(verb, url, body, callback) {
 
 function delay(ms) {
   return new Promise(resolve => {
-    setTimeout(() => resolve(), ms);
-  });
+    setTimeout(() => resolve(), ms)
+  })
 }
