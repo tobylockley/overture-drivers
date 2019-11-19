2ef1cce6173b0589a0e12907a2b13d363c250a6d
Toby Lockley <toby@avdistributors.com>
2019-11-19 10:19:36 +1000

    Updated from latest tower irdb import template

--- drivers/zeevee_zvsync/index.js
+++ drivers/zeevee_zvsync/index.js
@@ -1,3 +1,11 @@
+'use strict'
+
+const CMD_DEFER_TIME = 3000 // Timeout when using commandDefer
+const TICK_PERIOD = 5000 // In-built tick interval
+const POLL_PERIOD = 20000 // Continuous polling interval used for keepAlive
+const TCP_TIMEOUT = 30000 // Will timeout after this length of inactivity
+const TCP_RECONNECT_DELAY = 3000 // How long to wait before attempting to reconnect
+
 let host
 exports.init = _host => {
   host = _host
@@ -7,99 +15,133 @@ exports.createDevice = base => {
   const logger = base.logger || host.logger
   let config
   let tcpClient
+  const ir_codes = require('./ir_codes.json')
 
-  base.setTickPeriod(5000)
+  // ------------------------------ SETUP FUNCTIONS ------------------------------
 
-  const tick = () => {
-    if (base.getVar('Status').string == 'Disconnected') {
-      initTcpClient()
-      tcpClient.connect(config.port, config.host)
-    }
+  function isConnected() {
+    return base.getVar('Status').string === 'Connected'
   }
 
-  const setup = _config => {
+  function setup(_config) {
     config = _config
-    base.setPoll('Keep Alive', 30000)
+    base.setTickPeriod(TICK_PERIOD)
+
+    // Register polling functions
+    base.setPoll({
+      action: 'keepAlive',
+      period: POLL_PERIOD,
+      enablePollFn: isConnected,
+      startImmediately: true
+    })
+
+    base.getVar('IR_Commands').enums = ['Idle'].concat(Object.keys(ir_codes))
   }
 
-  const start = () => {
+  function start() {
     initTcpClient()
+  }
+
+  function stop() {
+    base.clearPendingCommands()
+    base.getVar('Status').string = 'Disconnected'
+    tcpClient && tcpClient.end()
+    tcpClient = null
+  }
+
+  function tick() {
+    if (!tcpClient) initTcpClient()
+  }
+
+  function initTcpClient() {
+    if (tcpClient) return // Return if tcpClient already exists
+
+    tcpClient = host.createTCPClient()
+    tcpClient.setOptions({
+      receiveTimeout: TCP_TIMEOUT,
+      autoReconnectionAttemptDelay: TCP_RECONNECT_DELAY
+    })
     tcpClient.connect(config.port, config.host)
-    base.startPolling()
+
+    tcpClient.on('connect', () => {
+      logger.silly('TCPClient connected')
+      base.getVar('Status').string = 'Connected'
+      base.startPolling()
+    })
+
+    tcpClient.on('data', data => {
+      onFrame(data.toString())
+    })
+
+    tcpClient.on('close', () => {
+      logger.silly('TCPClient closed')
+      base.getVar('Status').string = 'Disconnected' // Triggered on timeout, this allows auto reconnect
+    })
+
+    tcpClient.on('error', err => {
+      logger.error(`TCPClient: ${err}`)
+      stop() // Throw out the tcpClient and get a fresh connection
+    })
   }
 
-  const stop = () => {
-    disconnect()
-  }
+  // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
 
-  let ir_commands = require('./ir_codes.js')
-
-  const sendCommand = params => {
-    // base.getVar("IRCommands").string = params.Name;
-    let ircode = ir_commands[params.Name.replace(/\s/g, '_').toUpperCase()]
-    if (ircode) {
-      sendDefer(`sendir,${config.module}:${config.ir_port},${ircode}\r`)
-    }
-    else {
-      logger.debug('Wrong command variable sent to function Send Command()')
-    }
-  }
-
-  const keepAlive = () => sendDefer('A\r')
-
-  const onFrame = data => {
-    logger.silly(`onFrame ${data}`)
-    base.commandDone()
-  }
-
-  const initTcpClient = () => {
-    if (!tcpClient) {
-      tcpClient = host.createTCPClient()
-
-      tcpClient.on('connect', () => {
-        logger.silly('TCPClient connected')
-        base.getVar('Status').string = 'Connected'
-      })
-
-      tcpClient.on('data', data => {
-        data = data.toString()
-        logger.silly(`TCPClient data: ${data}`)
-        onFrame(data)
-      })
-
-      tcpClient.on('close', () => {
-        logger.silly('TCPClient closed')
-        disconnect()
-      })
-
-      tcpClient.on('error', err => {
-        logger.error(`TCPClient: ${err}`)
-        disconnect()
-      })
-    }
-  }
-
-  const sendDefer = data => {
-    if (send(data)) {
-      base.commandDefer()
-    }
-    else {
-      base.commandError('Data not sent')
-    }
-  }
-
-  const send = data => {
+  function send(data) {
     logger.silly(`TCPClient send: ${data}`)
     return tcpClient && tcpClient.write(data)
   }
 
-  const disconnect = () => {
-    //base.getVar('Status').string = 'Disconnected'
-    //tcpClient && tcpClient.end()
+  function sendDefer(data) {
+    base.commandDefer(CMD_DEFER_TIME)
+    if (!send(data)) base.commandError('Data not sent')
   }
 
+  function onFrame(data) {
+    const pendingCommand = base.getPendingCommand()
+    if (pendingCommand && pendingCommand.action === 'sendCommand') {
+      const cmdName = base.getVar('IR_Commands').enums[
+        pendingCommand.params.Index
+      ]
+      logger.silly(`pendingCommand: ${cmdName}  -  received: ${data}`)
+      base.getVar('IR_Commands').value = 0 // Set back to idle
+      base.commandDone()
+    }
+    if (pendingCommand && pendingCommand.action === 'keepAlive') {
+      base.commandDone()
+    }
+  }
+
+  // ------------------------------ DEVICE FUNCTIONS ------------------------------
+
+  function sendCommand(params) {
+    let name = base.getVar('IR_Commands').enums[params.Index]
+    let code = ir_codes[name]
+    if (code) {
+      base.getVar('IR_Commands').value = params.Index
+      sendDefer(`sendir,${config.module}:${config.ir_port},${code}\r`)
+      setTimeout(function() {
+        let v = base.getVar('IR_Commands')
+        if (v.value !== 0) v.value = 0 // Set back to idle
+      }, 1)
+    }
+    else {
+      logger.error(
+        `Invalid command index sent to function sendCommand: ${params.Index}`
+      )
+    }
+  }
+
+  function keepAlive() {
+    sendDefer('getversion\r')
+  }
+
+  // ------------------------------ EXPORTED FUNCTIONS ------------------------------
   return {
-    setup, start, stop,
-    keepAlive, sendCommand
+    setup,
+    start,
+    stop,
+    tick,
+    sendCommand,
+    keepAlive
   }
 }
