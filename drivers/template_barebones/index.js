ead578f47609a6fd4c6b50293615359d52b20c34
Toby Lockley <toby@avdistributors.com>
2020-04-01 14:31:25 +1100

    Absolute minimum boilerplate for a driver

--- drivers/template_barebones/index.js
+++ drivers/template_barebones/index.js
@@ -1,38 +1,28 @@
-'use strict'
-
 let host
 exports.init = _host => {
-  host = _host
+    host = _host
 }
 
-
 exports.createDevice = base => {
-  const logger = base.logger || host.logger
-  let config
+    const logger = base.logger || host.logger
+    let config
 
+    //------------------------------------------------------------------------- STANDARD SDK FUNCTIONS
+    function setup(_config) {
+        config = _config
+    }
 
-  // ------------------------------ BASE FUNCTIONS ------------------------------
+    function start() {
+    }
 
-  function setup(_config) {
-    config = _config
-    logger.silly(config)  // To remove eslint errors. Should be removed.
-  }
+    function stop() {
+    }
 
+    function tick() {
+    }
 
-  function start() {
-  }
-
-
-  function stop() {
-  }
-
-
-  function tick() {
-  }
-
-
-  // ------------------------------ EXPORTED FUNCTIONS ------------------------------
-  return {
-    setup, start, stop, tick
-  }
+    // ------------------------------ EXPORTED FUNCTIONS ------------------------------
+    return {
+        setup, start, stop, tick
+    }
 }
