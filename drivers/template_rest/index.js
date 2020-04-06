9fbb12b564302433c3df82c41826ca74fe189632
Toby Lockley <toby@avdistributors.com>
2020-04-06 11:36:59 +1000

    Updated REST template

--- drivers/template_rest/index.js
+++ drivers/template_rest/index.js
@@ -1,8 +1,7 @@
-'use strict'
-
-const CMD_DEFER_TIME = 3000        // Timeout when using commandDefer
-const TICK_PERIOD = 10000          // In-built tick interval
-const POLL_PERIOD = 5000           // Continuous polling function interval
+//------------------------------------------------------------------------------------------ CONSTANTS
+const CMD_DEFER_TIME = 3000         // Timeout when using commandDefer
+const POLL_PERIOD = 5000            // Polling function interval
+const TICK_PERIOD = 5000            // In-built tick interval
 const REQUEST_TIMEOUT = 2000       // Timeout for AJAX requests
 
 let host
@@ -14,32 +13,23 @@ exports.createDevice = base => {
     const logger = base.logger || host.logger
     let config
 
-
-    // ------------------------------ SETUP FUNCTIONS ------------------------------
-
-    function isConnected() { return base.getVar('Status').string === 'Connected' }
-
+    //------------------------------------------------------------------------- STANDARD SDK FUNCTIONS
     function setup(_config) {
         config = _config
         base.setTickPeriod(TICK_PERIOD)
         // Register polling functions
-        base.setPoll({ action: 'getPower', period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true })
+        const defaults = {period: POLL_PERIOD, enablePollFn: isConnected, startImmediately: true}
+        base.setPoll({...defaults, action: 'getPower'})
     }
 
     function start() {
-        base.startPolling()
         tick()  // Get the connection state straight away
-    }
-
-    function stop() {
-        base.getVar('Status').string = 'Disconnected'
-        base.stopPolling()
-        base.clearPendingCommands()
+        base.startPolling()
     }
 
     async function tick() {
         try {
-            let response = await req('info')  // If no error is thrown, connection is valid
+            await restGet('/v1.0/Software/FirmwareVersion')  // If no error is thrown, connection is valid
             if (base.getVar('Status').string === 'Disconnected') {
                 base.getVar('Status').string = 'Connected'
             }
@@ -50,51 +40,82 @@ exports.createDevice = base => {
         }
     }
 
+    function stop() {
+        base.getVar('Status').string = 'Disconnected'
+        base.stopPolling()
+        base.clearPendingCommands()
+    }
 
-    // ------------------------------ SEND/RECEIVE HANDLERS ------------------------------
-
-    async function req(cmdString) {
+    //-------------------------------------------------------------------------- SEND/RECEIVE HANDLERS
+    async function restGet(path) {
         base.commandDefer(CMD_DEFER_TIME)
         try {
-            logger.silly(`Running REST request > ${cmdString}`)
+            logger.silly('GET request:', path)
             const options = {
-                method: 'POST',
-                uri: `http://${config.host}/rcCmd.php`,
+                method: 'GET',
+                uri: `http://${config.host}${path}`,
+                timeout: REQUEST_TIMEOUT
+            }
+            let response = await host.request(options)
+            response = JSON.parse(response)
+            logger.silly('GET response:', response)
+            base.commandDone()
+            return response
+        }
+        catch (error) {
+            base.commandError(error.message)
+            throw new Error(`req failed > ${error.message}`)
+        }
+    }
+
+    async function restPut(path, value) {
+        base.commandDefer(CMD_DEFER_TIME)
+        try {
+            logger.silly('PUT request:', path, value)
+            const options = {
+                method: 'PUT',
+                uri: `http://${config.host}${path}`,
                 timeout: REQUEST_TIMEOUT,
                 form: {
-                    commands: cmdString
+                    value: value
                 }
             }
             let response = await host.request(options)
             response = JSON.parse(response)
-            let zyperResponse = response.responses[0]
-            for (let warning of zyperResponse.warnings) logger.warn(`zyperCmd warning > ${warning}`)
-            if (zyperResponse.errors.length > 0) throw new Error(zyperResponse.errors[0])
+            logger.silly('PUT response:', response)
             base.commandDone()
-            return zyperResponse
+            return response
         }
         catch (error) {
             base.commandError(error.message)
-            throw new Error(`zyperCmd failed > ${error.message}`)
+            throw new Error(`restPut failed: ${error.message}`)
         }
     }
 
-
-    // ------------------------------ GET FUNCTIONS ------------------------------
-
+    //---------------------------------------------------------------------------------- GET FUNCTIONS
     function getPower() {
-        sendDefer('*SEPOWR################\n')
+        restGet('/v1.0/Display/StandbyState').then(response => {
+            // Process response
+        })
     }
 
-
-    // ------------------------------ SET FUNCTIONS ------------------------------
+    //---------------------------------------------------------------------------------- SET FUNCTIONS
     function setPower(params) {
-        if (params.Status == 'Off') sendDefer('*SCPOWR0000000000000000\n')
-        else if (params.Status == 'On') sendDefer('*SCPOWR0000000000000001\n')
+        let state
+        if (params.Status == 'Off') state = false
+        else if (params.Status == 'On') state = true
+        restPut('/v1.0/Display/StandbyState', state).then(response => {
+            // Process response
+            // base.getVar('Power').string == 'Off'
+        })
     }
 
+    //------------------------------------------------------------------------------- HELPER FUNCTIONS
+    function isConnected() {
+        return base.getVar('Status').string == 'Connected'
+    }
 
-    // ------------------------------ EXPORTED FUNCTIONS ------------------------------
+    //----------------------------------------------------------------------------- EXPORTED FUNCTIONS
     return {
         setup, start, stop, tick,
         getPower,
